5af17b80ad53e2dba68f3faaa565da5d1961fc39
Alan Jowett <alanjo@microsoft.com>
2021-03-15 18:46:56 +0000

    Merged PR 4244102: Tweak for demo
    Tweak for demo

--- src/tools/port_leak/port_leak.cpp
+++ src/tools/port_leak/port_leak.cpp
@@ -12,9 +12,9 @@ main()
 {
     WSADATA wsa_data;
     WSAStartup(2, &wsa_data);
+    int leaked_ports = 0;
 
     for (;;) {
-        Sleep(10);
 
         auto s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
         if (s == INVALID_SOCKET) {
@@ -24,6 +24,26 @@ main()
         addr.sin_addr.S_un.S_addr = 0;
         addr.sin_port = 0;
         addr.sin_family = AF_INET;
-        bind(s, (sockaddr*)&addr, sizeof(addr));
+        if (bind(s, (sockaddr*)&addr, sizeof(addr)) != SOCKET_ERROR) {
+            leaked_ports++;
+            if (leaked_ports % 100 == 0) {
+                printf("Leaked %d ports\n", leaked_ports);
+                Sleep(10);
+            }
+        } else {
+            DWORD err = WSAGetLastError();
+            wchar_t* s = NULL;
+            FormatMessageW(
+                FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                NULL,
+                WSAGetLastError(),
+                MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                (LPWSTR)&s,
+                0,
+                NULL);
+            printf("bind failed: %S\t%d\n", s, WSAGetLastError());
+            LocalFree(s);
+            Sleep(1000);
+        }
     }
 }
