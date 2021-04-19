3045151f799527cbfdc95c22bc790fd836eb481f
Alan Jowett <alanjo@microsoft.com>
2021-04-19 09:13:08 -0600

    Update src/ebpf/include/ebpf_epoch.h
    Co-authored-by: Dave Thaler <dthaler@microsoft.com>

--- src/ebpf/include/ebpf_epoch.h
+++ src/ebpf/include/ebpf_epoch.h
@@ -33,6 +33,7 @@ extern "C"
      * @brief Allocate memory under epoch control.
      * @param[in] size Size of memory to allocate
      * @param[in] type Allocate memory as executable vs non-executable
+     * @returns Pointer to memory block allocated, or null on failure.
      */
     void*
     ebpf_epoch_allocate(size_t size, ebpf_memory_type_t type);
