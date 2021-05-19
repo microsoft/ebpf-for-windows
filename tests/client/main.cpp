2f11e3a2e25d402f760411820c92ccb2262f89c8
Anurag Saxena <43585259+saxena-anurag@users.noreply.github.com>
2021-05-19 00:37:22 -0700

    test code refactoring

--- tests/client/main.cpp
+++ tests/client/main.cpp
@@ -24,27 +24,22 @@
 int
 ebpf_rpc_verify_program(ebpf_program_verify_info* info, unsigned char** logs, uint32_t* logs_size);
 
-TEST_CASE("verify-program-droppacket", "[verify-program-droppacket]")
+void
+_get_program_byte_code_helper(const char* file_name, const char* section_name, ebpf_program_verify_info* info)
 {
     uint8_t* instruction_array;
     uint32_t instruction_array_size;
-    EbpfMapDescriptor* descriptors;
+    EbpfMapDescriptor* descriptors = nullptr;
     int descriptors_count;
     uint32_t result = ERROR_SUCCESS;
     ebpf_program_type_t program_type;
     const char* error_message = nullptr;
-    unsigned char* verifier_message = nullptr;
-    uint32_t verifier_message_size;
-    ebpf_program_verify_info info = {0};
-
-    service_install_helper service_helper(EBPF_SERVICE_NAME, EBPF_SERVICE_BINARY_NAME);
-    REQUIRE(service_helper.initialize() == ERROR_SUCCESS);
 
     // Get byte code and map descriptors from ELF file.
     REQUIRE(
         (result = ebpf_get_program_byte_code(
-             "droppacket.o",
-             "xdp",
+             file_name,
+             section_name,
              &program_type,
              true, // mock map fd
              &instruction_array,
@@ -59,36 +54,48 @@ TEST_CASE("verify-program-droppacket", "[verify-program-droppacket]")
 
     REQUIRE(instruction_array_size != 0);
 
-    info.program_type = program_type;
-    info.byte_code = instruction_array;
-    info.byte_code_size = instruction_array_size;
-    info.map_descriptors_count = descriptors_count;
-    info.map_descriptors = reinterpret_cast<ebpf_map_descriptor*>(descriptors);
+    info->program_type = program_type;
+    info->byte_code = instruction_array;
+    info->byte_code_size = instruction_array_size;
+    info->map_descriptors_count = descriptors_count;
+    if (descriptors != nullptr) {
+        info->map_descriptors = reinterpret_cast<ebpf_map_descriptor*>(descriptors);
+    }
 
     printf("instruction_array_size = %d\n", instruction_array_size);
+    ebpf_api_free_string(error_message);
+}
+
+TEST_CASE("verify-program-droppacket", "[verify-program-droppacket]")
+{
+    uint32_t result;
+    unsigned char* verifier_message = nullptr;
+    uint32_t verifier_message_size;
+    ebpf_program_verify_info info = {0};
+
+    service_install_helper service_helper(EBPF_SERVICE_NAME, EBPF_SERVICE_BINARY_NAME);
+    REQUIRE(service_helper.initialize() == ERROR_SUCCESS);
+
+    // Get byte code and map descriptors from ELF file.
+    _get_program_byte_code_helper("droppacket.o", "xdp", &info);
 
     REQUIRE(initialize_rpc_binding() == RPC_S_OK);
 
     REQUIRE(
         (result = ebpf_rpc_verify_program(&info, &verifier_message, &verifier_message_size),
-         error_message ? printf("ebpf_get_program_byte_code failed with %s\n", error_message) : 0,
-         ebpf_api_free_string(error_message),
-         error_message = nullptr,
+         verifier_message ? printf("ebpf_get_program_byte_code failed with %s\n", verifier_message) : 0,
+         ebpf_api_free_string((const char*)verifier_message),
+         verifier_message = nullptr,
          result == ERROR_SUCCESS));
 
+    ebpf_api_free_string((const char*)verifier_message);
     clean_up_rpc_binding();
     service_helper.uninitialize();
 }
 
 TEST_CASE("verify-program-divide_by_zero", "[verify-program-divide_by_zero]")
 {
-    uint8_t* instruction_array;
-    uint32_t instruction_array_size;
-    EbpfMapDescriptor* descriptors;
-    int descriptors_count;
-    uint32_t result = ERROR_SUCCESS;
-    ebpf_program_type_t program_type;
-    const char* error_message = nullptr;
+    uint32_t result;
     unsigned char* verifier_message = nullptr;
     uint32_t verifier_message_size;
     ebpf_program_verify_info info = {0};
@@ -97,54 +104,25 @@ TEST_CASE("verify-program-divide_by_zero", "[verify-program-divide_by_zero]")
     REQUIRE(service_helper.initialize() == ERROR_SUCCESS);
 
     // Get byte code and map descriptors from ELF file.
-    REQUIRE(
-        (result = ebpf_get_program_byte_code(
-             "divide_by_zero.o",
-             "xdp",
-             &program_type,
-             true, // mock map fd
-             &instruction_array,
-             &instruction_array_size,
-             &descriptors,
-             &descriptors_count,
-             &error_message),
-         error_message ? printf("ebpf_get_program_byte_code failed with %s\n", error_message) : 0,
-         ebpf_api_free_string(error_message),
-         error_message = nullptr,
-         result == ERROR_SUCCESS));
-
-    REQUIRE(instruction_array_size != 0);
-
-    info.program_type = program_type;
-    info.byte_code = instruction_array;
-    info.byte_code_size = instruction_array_size;
-    info.map_descriptors_count = descriptors_count;
-    info.map_descriptors = reinterpret_cast<ebpf_map_descriptor*>(descriptors);
-
-    printf("instruction_array_size = %d\n", instruction_array_size);
+    _get_program_byte_code_helper("divide_by_zero.o", "xdp", &info);
 
     REQUIRE(initialize_rpc_binding() == RPC_S_OK);
 
     REQUIRE(
         (result = ebpf_rpc_verify_program(&info, &verifier_message, &verifier_message_size),
-         error_message ? printf("ebpf_get_program_byte_code failed with %s\n", error_message) : 0,
-         ebpf_api_free_string(error_message),
-         error_message = nullptr,
+         verifier_message ? printf("ebpf_get_program_byte_code failed with %s\n", verifier_message) : 0,
+         ebpf_api_free_string((const char*)verifier_message),
+         verifier_message = nullptr,
          result == ERROR_SUCCESS));
 
+    ebpf_api_free_string((const char*)verifier_message);
     clean_up_rpc_binding();
     service_helper.uninitialize();
 }
 
 TEST_CASE("verify-program-droppacket_unsafe", "[verify-program-droppacket_unsafe]")
 {
-    uint8_t* instruction_array;
-    uint32_t instruction_array_size;
-    EbpfMapDescriptor* descriptors;
-    int descriptors_count;
-    uint32_t result = ERROR_SUCCESS;
-    ebpf_program_type_t program_type;
-    const char* error_message = nullptr;
+    uint32_t result;
     unsigned char* verifier_message = nullptr;
     uint32_t verifier_message_size;
     ebpf_program_verify_info info = {0};
@@ -153,31 +131,7 @@ TEST_CASE("verify-program-droppacket_unsafe", "[verify-program-droppacket_unsafe
     REQUIRE(service_helper.initialize() == ERROR_SUCCESS);
 
     // Get byte code and map descriptors from ELF file.
-    REQUIRE(
-        (result = ebpf_get_program_byte_code(
-             "droppacket_unsafe.o",
-             "xdp",
-             &program_type,
-             true, // mock map fd
-             &instruction_array,
-             &instruction_array_size,
-             &descriptors,
-             &descriptors_count,
-             &error_message),
-         error_message ? printf("ebpf_get_program_byte_code failed with %s\n", error_message) : 0,
-         ebpf_api_free_string(error_message),
-         error_message = nullptr,
-         result == ERROR_SUCCESS));
-
-    REQUIRE(instruction_array_size != 0);
-
-    info.program_type = program_type;
-    info.byte_code = instruction_array;
-    info.byte_code_size = instruction_array_size;
-    info.map_descriptors_count = descriptors_count;
-    info.map_descriptors = reinterpret_cast<ebpf_map_descriptor*>(descriptors);
-
-    printf("instruction_array_size = %d\n", instruction_array_size);
+    _get_program_byte_code_helper("droppacket_unsafe.o", "xdp", &info);
 
     REQUIRE(initialize_rpc_binding() == RPC_S_OK);
 
@@ -190,7 +144,9 @@ TEST_CASE("verify-program-droppacket_unsafe", "[verify-program-droppacket_unsafe
         }
     }
     REQUIRE((result == (int)EBPF_VALIDATION_FAILED));
+    REQUIRE(verifier_message_size > 0);
 
+    ebpf_api_free_string((const char*)verifier_message);
     clean_up_rpc_binding();
     service_helper.uninitialize();
 }
