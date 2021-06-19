3448a8fb540fb20588311322651a2d824518fcd9
Anurag Saxena <43585259+saxena-anurag@users.noreply.github.com>
2021-06-18 23:13:47 -0700

    fix platform test

--- libs/platform/unit/platform_unit_test.cpp
+++ libs/platform/unit/platform_unit_test.cpp
@@ -235,11 +235,10 @@ TEST_CASE("program_type_info", "[program_type_info]")
          EBPF_RETURN_TYPE_PTR_TO_MAP_VALUE_OR_NULL,
          {EBPF_ARGUMENT_TYPE_PTR_TO_MAP, EBPF_ARGUMENT_TYPE_PTR_TO_MAP_KEY}},
     };
-    ebpf_context_descriptor_t context_descriptor{
-        sizeof(xdp_md_t),
-        EBPF_OFFSET_OF(xdp_md_t, data),
-        EBPF_OFFSET_OF(xdp_md_t, data_end),
-        EBPF_OFFSET_OF(xdp_md_t, data_meta)};
+    ebpf_context_descriptor_t context_descriptor{sizeof(xdp_md_t),
+                                                 EBPF_OFFSET_OF(xdp_md_t, data),
+                                                 EBPF_OFFSET_OF(xdp_md_t, data_end),
+                                                 EBPF_OFFSET_OF(xdp_md_t, data_meta)};
     ebpf_program_type_descriptor_t program_type{"xdp", &context_descriptor};
     ebpf_program_information_t program_information{program_type, _countof(helper_functions), helper_functions};
     ebpf_program_information_t* new_program_information = nullptr;
@@ -269,7 +268,7 @@ TEST_CASE("program_type_info_stored", "[program_type_info_stored]")
             &bind_program_information,
             _ebpf_encoded_bind_program_information_data,
             sizeof(_ebpf_encoded_bind_program_information_data)) == EBPF_SUCCESS);
-    REQUIRE(strcmp(xdp_program_information->program_type_descriptor.name, "bind") == 0);
+    REQUIRE(strcmp(bind_program_information->program_type_descriptor.name, "bind") == 0);
     REQUIRE(bind_program_information->count_of_helpers == 3);
     ebpf_free(bind_program_information);
 }
