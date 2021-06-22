007c2a0189449205dae6852a67dbe2de3fa3a026
Dave Thaler <dthaler@ntdev.microsoft.com>
2021-06-22 13:32:43 -0700

    Add some netsh tests
    Addresses part of issue #240
    
    Signed-off-by: Dave Thaler <dthaler@ntdev.microsoft.com>

---
+++ tests/end_to_end/capture_helper.cpp
@@ -0,0 +1,105 @@
+// Copyright (c) Microsoft Corporation
+// SPDX-License-Identifier: MIT
+
+#include <fcntl.h>
+#include <fstream>
+#include <io.h>
+#include <iostream>
+#include <sal.h>
+#include <sstream>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <windows.h>
+#include "capture_helper.hpp"
+
+// This class can be used by tests to catch stdout and stderr output.
+
+#define TEMPORARY_STDOUT_FILENAME ".\\stdout.txt"
+#define TEMPORARY_STDERR_FILENAME ".\\stderr.txt"
+
+std::string
+capture_helper_t::get_stdout_contents(void)
+{
+    end_capture();
+    return _stdout_contents;
+}
+
+std::string
+capture_helper_t::get_stderr_contents(void)
+{
+    end_capture();
+    return _stderr_contents;
+}
+
+errno_t
+capture_helper_t::begin_fd_capture(_In_ FILE* fp, _Out_ int* original_fd, _In_z_ const char* temporary_filename)
+{
+    // Create a temporary file.
+    int destination_fd;
+    errno_t err =
+        _sopen_s(&destination_fd, temporary_filename, _O_WRONLY | O_CREAT | O_TRUNC, _SH_DENYNO, _S_IREAD | _S_IWRITE);
+    if (err != 0) {
+        return err;
+    }
+
+    int standard_fileno = _fileno(fp);
+    fflush(fp);
+    *original_fd = _dup(standard_fileno);
+
+    // Redirect fp to the temporary file.
+    _dup2(destination_fd, standard_fileno);
+    _close(destination_fd);
+    return 0;
+}
+
+std::string
+capture_helper_t::end_fd_capture(_In_ FILE* fp, _Inout_ int* original_fd, _In_z_ const char* temporary_filename)
+{
+    if (*original_fd == -1) {
+        // Nothing to do.
+        return {};
+    }
+
+    // Restore standard fd to original.
+    int standard_fileno = _fileno(fp);
+    fflush(fp);
+    _dup2(*original_fd, standard_fileno);
+    _close(*original_fd);
+    *original_fd = -1;
+
+    // Read the contents of the temporary file.
+    std::ifstream stdout_fs(temporary_filename);
+    std::string contents((std::istreambuf_iterator<char>(stdout_fs)), std::istreambuf_iterator<char>());
+
+    // Clean up the temporary file.
+    _unlink(temporary_filename);
+
+    return contents;
+}
+
+errno_t
+capture_helper_t::begin_capture(void)
+{
+    errno_t error = begin_fd_capture(stdout, &_original_stdout_fd, TEMPORARY_STDOUT_FILENAME);
+    if (error != 0) {
+        return error;
+    }
+
+    error = begin_fd_capture(stderr, &_original_stderr_fd, TEMPORARY_STDERR_FILENAME);
+    if (error != 0) {
+        end_fd_capture(stdout, &_original_stdout_fd, TEMPORARY_STDOUT_FILENAME);
+        return error;
+    }
+
+    return 0;
+}
+
+void
+capture_helper_t::end_capture(void)
+{
+    _stdout_contents = end_fd_capture(stdout, &_original_stdout_fd, TEMPORARY_STDOUT_FILENAME);
+    _stderr_contents = end_fd_capture(stderr, &_original_stderr_fd, TEMPORARY_STDERR_FILENAME);
+}
+
+capture_helper_t::~capture_helper_t(void) { end_capture(); }
