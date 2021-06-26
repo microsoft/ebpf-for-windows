d84adfd165091237cc7cc32a1bb68ff29e665195
Alan Jowett <alanjo@microsoft.com>
2021-06-26 16:51:50 -0600

    PR feedback
    Signed-off-by: Alan Jowett <alanjo@microsoft.com>

--- libs/platform/ebpf_platform.h
+++ libs/platform/ebpf_platform.h
@@ -549,12 +549,12 @@ extern "C"
      * @param[out] client_context Context used to unload the extension.
      * @param[in] interface_id GUID representing the identity of the interface.
      * @param[in] client_binding_context Opaque per-instance pointer passed to the extension.
-     * @param[in] client_data Opaque client data passed to the extension.
+     * @param[in] client_data Opaque client data passed to the extension or
+        NULL if there is none.
      * @param[in] client_data_length Length of the client data.
      * @param[in] client_dispatch_table Table of function pointers the client
-     *  exposes.
-     * @param[out] provider_binding_context Provider binding context or NULL if
-        the caller doesn't care.
+     *  exposes or NULL if there is none.
+     * @param[out] provider_binding_context Provider binding context. Can be NULL.
      * @param[out] provider_data Opaque provider data.
      * @param[out] provider_dispatch_table Table of function pointers the
      *  provider exposes.
