4e70a09b8499ab32496686ee627c7fdbf465109e
Trish Lam <t-trishlam@microsoft.com>
2021-08-05 23:40:03 -0700

    Reformat ebpf progs to OS repo clang format

--- tests/sample/associatetoflow.c
+++ tests/sample/associatetoflow.c
@@ -10,19 +10,17 @@
 
 #pragma clang section data = "maps"
 ebpf_map_definition_t app_map = {
-    .size = sizeof(ebpf_map_definition_t),
-    .type = BPF_MAP_TYPE_HASH,
-    .key_size = sizeof(five_tuple_t),
-    .value_size = sizeof(uint64_t),
-    .max_entries = 500};
+    .size = sizeof(ebpf_map_definition_t), .type = BPF_MAP_TYPE_HASH, .key_size = sizeof(five_tuple_t), .value_size = sizeof(uint64_t), .max_entries = 500};
 
 #pragma clang section text = "flow"
-void
-AssociateFlowToContext(flow_md_t* context)
+void AssociateFlowToContext(flow_md_t* context)
 {
-    if (context->flow_established_flag) {
+    if (context->flow_established_flag)
+    {
         bpf_map_update_elem(&app_map, &context->five_tuple, &context->app_id, NO_FLAGS);
-    } else { // flow deletion
+    }
+    else
+    { // flow deletion
         bpf_map_delete_elem(&app_map, &context->five_tuple);
     }
     return;
--- tests/sample/countbytes.c
+++ tests/sample/countbytes.c
@@ -10,20 +10,18 @@
 
 #pragma clang section data = "maps"
 ebpf_map_definition_t byte_map = {
-    .size = sizeof(ebpf_map_definition_t),
-    .type = BPF_MAP_TYPE_HASH,
-    .key_size = sizeof(five_tuple_t),
-    .value_size = sizeof(uint64_t),
-    .max_entries = 500};
+    .size = sizeof(ebpf_map_definition_t), .type = BPF_MAP_TYPE_HASH, .key_size = sizeof(five_tuple_t), .value_size = sizeof(uint64_t), .max_entries = 500};
 
 #pragma clang section text = "mac"
-void
-CountBytes(mac_md_t* context)
+void CountBytes(mac_md_t* context)
 {
     uint64_t* byte_count = bpf_map_lookup_elem(&byte_map, &context->five_tuple);
-    if (!byte_count) {
+    if (!byte_count)
+    {
         bpf_map_update_elem(&byte_map, &context->five_tuple, context->packet_length, NO_FLAGS);
-    } else {
+    }
+    else
+    {
         bpf_map_update_elem(&byte_map, &context->five_tuple, *byte_count + context->packet_length, NO_FLAGS);
     }
     return;
