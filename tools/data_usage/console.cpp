a35d3fda56bde4d3f6fa2a9cbd7143bd10b4437d
Trish Lam <t-trishlam@microsoft.com>
2021-08-25 11:03:09 -0700

    Console and hook callouts working with newer upstream commits

--- tools/data_usage/console.cpp
+++ tools/data_usage/console.cpp
@@ -1,10 +1,19 @@
 // Copyright (c) Microsoft Corporation
 // SPDX-License-Identifier: MIT
 
-#include <windows.h>
+// #include <windows.h>
+// #include <iostream>
+// #include <string>
+// #include "ebpf_api.h"
+// #include "ebpf_nethooks.h"
+
+
 #include <iostream>
 #include <string>
+#include <windows.h>
+#include "bpf.h"
 #include "ebpf_api.h"
+#include "libbpf.h"
 #include "ebpf_nethooks.h"
 
 #define MAP_COUNT 1
@@ -14,54 +23,57 @@ int main(int argc, const char** argv)
     UNREFERENCED_PARAMETER(argc);
     UNREFERENCED_PARAMETER(argv);
 
-    ebpf_handle_t flow_program_handle;
-    ebpf_handle_t flow_map_handle[MAP_COUNT];
-    ebpf_handle_t flow_link_handle;
-
-    ebpf_handle_t mac_program_handle;
-    ebpf_handle_t mac_map_handle[MAP_COUNT];
-    ebpf_handle_t mac_link_handle;
-
-    uint32_t count_of_map_handle = MAP_COUNT;
-    const char* error_message = nullptr;
-
-    uint32_t result = ebpf_api_initiate();
-    if (result != ERROR_SUCCESS)
+    uint32_t initiate_result = ebpf_api_initiate();
+    if (initiate_result != ERROR_SUCCESS)
     {
-        std::cerr << "ebpf_api_initiate failed: " << result << std::endl;
+        std::cerr << "ebpf_api_initiate failed: " << initiate_result << std::endl;
         return 1;
     }
 
-    result = ebpf_api_load_program(
-        "associatetoflow.o", "flow", EBPF_EXECUTION_JIT, &flow_program_handle, &count_of_map_handle, flow_map_handle, &error_message);
+    const char* error_message = NULL;
+    ebpf_result_t result;
+    bpf_object* flow_object = nullptr;
+    bpf_program* flow_program = nullptr;
+    bpf_link* flow_link = nullptr;
+    fd_t flow_program_fd;
 
-    if (result != ERROR_SUCCESS)
-    {
+    result = ebpf_program_load(
+        "associatetoflow.o", &EBPF_PROGRAM_TYPE_FLOW, &EBPF_ATTACH_TYPE_FLOW, EBPF_EXECUTION_JIT, &flow_object, &flow_program_fd, &error_message);
+    if (result != EBPF_SUCCESS) {
         std::cerr << "Failed to load associatetoflow eBPF program: " << error_message << std::endl;
         ebpf_free_string(error_message);
         return 1;
     }
-
-    result = ebpf_api_link_program(flow_program_handle, EBPF_ATTACH_TYPE_FLOW, &flow_link_handle);
-    if (result != ERROR_SUCCESS)
-    {
+    flow_program = bpf_program__next(nullptr, flow_object);
+    if (flow_program == nullptr) {
+        std::cerr << "Failed to find associatetoflow eBPF program from object." << std::endl;
+        return 1;
+    }
+    result = ebpf_program_attach(flow_program, &EBPF_ATTACH_TYPE_FLOW, nullptr, 0, &flow_link);
+    if (result != ERROR_SUCCESS) {
         std::cerr << "Failed to attach associatetoflow eBPF program: " << result << std::endl;
         return 1;
     }
 
-    result = ebpf_api_load_program(
-        "countbytes.o", "mac", EBPF_EXECUTION_JIT, &mac_program_handle, &count_of_map_handle, mac_map_handle, &error_message);
+    bpf_object* mac_object = nullptr;
+    bpf_program* mac_program = nullptr;
+    bpf_link* mac_link = nullptr;
+    fd_t mac_program_fd;
 
-    if (result != ERROR_SUCCESS)
-    {
+    result = ebpf_program_load(
+        "countbytes.o", &EBPF_PROGRAM_TYPE_MAC, &EBPF_ATTACH_TYPE_MAC, EBPF_EXECUTION_JIT, &mac_object, &mac_program_fd, &error_message);
+    if (result != EBPF_SUCCESS) {
         std::cerr << "Failed to load countbytes eBPF program: " << error_message << std::endl;
         ebpf_free_string(error_message);
         return 1;
     }
-
-    result = ebpf_api_link_program(mac_program_handle, EBPF_ATTACH_TYPE_MAC, &mac_link_handle);
-    if (result != ERROR_SUCCESS)
-    {
+    mac_program = bpf_program__next(nullptr, mac_object);
+    if (mac_program == nullptr) {
+        std::cerr << "Failed to find countbytes eBPF program from object." << std::endl;
+        return 1;
+    }
+    result = ebpf_program_attach(mac_program, &EBPF_ATTACH_TYPE_MAC, nullptr, 0, &mac_link);
+    if (result != ERROR_SUCCESS) {
         std::cerr << "Failed to attach countbytes eBPF program: " << result << std::endl;
         return 1;
     }
@@ -69,6 +81,18 @@ int main(int argc, const char** argv)
     five_tuple_t key = {};
     app_name_t app_name = {};
     uint64_t byte_count = {};
+    int query_result;
+
+    fd_t flow_map_fd = bpf_object__find_map_fd_by_name(flow_object, "app_map");
+    if (flow_map_fd <= 0) {
+        std::cerr << "Failed to find eBPF map : app_map" << std::endl;
+        return 1;
+    }
+    fd_t mac_map_fd = bpf_object__find_map_fd_by_name(mac_object, "byte_map");
+    if (mac_map_fd <= 0) {
+        std::cerr << "Failed to find eBPF map : byte_map" << std::endl;
+        return 1;
+    }
 
     // Loop every 10 seconds to query data from both maps
     while (true)
@@ -76,39 +100,37 @@ int main(int argc, const char** argv)
         std::cout << "Querying..." << std::endl;
 
         // Get the first key of the map
-        result = ebpf_api_get_next_map_key(mac_map_handle[0], sizeof(five_tuple_t), NULL, reinterpret_cast<uint8_t*>(&key));
+        query_result = bpf_map_get_next_key(mac_map_fd, nullptr, reinterpret_cast<uint8_t*>(&key));
+        std::cerr << "Five-tuple:" << key.v4 << " " << key.protocol << " " << key.dest_port << " " << key.source_port << " " << key.dest_ip << " " << key.source_ip << std::endl;
 
         // Checks if next key exists
-        while (result == ERROR_SUCCESS)
+        while (query_result == ERROR_SUCCESS)
         {
             // Find application id value using the key
             memset(&app_name, 0, sizeof(app_name));
-            result = ebpf_api_map_find_element(
-                flow_map_handle[0], sizeof(five_tuple_t), reinterpret_cast<uint8_t*>(&key), sizeof(app_name_t), reinterpret_cast<uint8_t*>(&app_name));
-            if (result != ERROR_SUCCESS)
+            query_result = bpf_map_lookup_elem(flow_map_fd, reinterpret_cast<uint8_t*>(&key), reinterpret_cast<uint8_t*>(&app_name));
+            if (query_result != ERROR_SUCCESS)
             {
-                std::cerr << "Failed to look up eBPF map app_name entry: " << result << std::endl;
+                std::cerr << "Failed to look up eBPF map app_name entry: " << query_result << std::endl;
                 return 1;
             }
             std::cout << "- Application: " << app_name.name << std::endl;
 
             // Find byte count value using the key
             memset(&byte_count, 0, sizeof(byte_count));
-            result = ebpf_api_map_find_element(
-                mac_map_handle[0], sizeof(five_tuple_t), reinterpret_cast<uint8_t*>(&key), sizeof(uint64_t), reinterpret_cast<uint8_t*>(&byte_count));
-            if (result != ERROR_SUCCESS)
+            query_result = bpf_map_lookup_elem(mac_map_fd, reinterpret_cast<uint8_t*>(&key), reinterpret_cast<uint8_t*>(&byte_count));
+            if (query_result != ERROR_SUCCESS)
             {
-                std::cerr << "Failed to look up eBPF map byte_count entry: " << result << std::endl;
+                std::cerr << "Failed to look up eBPF map byte_count entry: " << query_result << std::endl;
                 return 1;
             }
             std::cout << "- Byte Count: " << byte_count << std::endl;
             std::cout << std::endl;
 
             // Get the next key and loop again
-            result = ebpf_api_get_next_map_key(
-                mac_map_handle[0], sizeof(five_tuple_t), reinterpret_cast<uint8_t*>(&key), reinterpret_cast<uint8_t*>(&key));
+            query_result = bpf_map_get_next_key(mac_map_fd, reinterpret_cast<uint8_t*>(&key), reinterpret_cast<uint8_t*>(&key));
         };
 
         Sleep(10000);
-    }
+   }
 }
