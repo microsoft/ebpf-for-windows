718269e7578faf407aa2f32e8c9ff802cf6947b8
Alan Jowett <alanjo@microsoft.com>
2021-08-28 12:35:39 -0600

    Code analysis fix
    Signed-off-by: Alan Jowett <alanjo@microsoft.com>

--- tests/performance/performance.cpp
+++ tests/performance/performance.cpp
@@ -34,7 +34,12 @@ _perf_epoch_enter_exit_alloc_free()
     ebpf_epoch_enter();
     void* p = ebpf_epoch_allocate(10);
     ebpf_epoch_free(p);
+    // Disable C6001
+    // We are intentionally modifying memory after free, but relying on epoch protection.
+#pragma warning(push)
+#pragma warning(disable : 6001)
     memset(p, 0xAA, 10);
+#pragma warning(pop)
     ebpf_epoch_exit();
 }
 
