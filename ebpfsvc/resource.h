ac9b6282b8df0dc3a94990f2da93870fb3d46a47
Alan Jowett <alanjo@microsoft.com>
2021-09-01 17:57:35 -0600

    Add version info to ebpfcore.sys, ebpfapi.dll, netebpfext.sys, and ebpfsvc.exe (#478)
    Signed-off-by: Alan Jowett <alanjo@microsoft.com>
    Co-authored-by: Dave Thaler <dthaler@microsoft.com>

---
+++ ebpfsvc/resource.h
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Corporation
+// SPDX-License-Identifier: MIT
+
+#define EBPF_FILE_DESCRIPTION "eBPF For Windows Service"
+#define EBPF_FILE_NAME "ebpfsvc.exe"
---
+++ netebpfext/resource.h
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Corporation
+// SPDX-License-Identifier: MIT
+
+#define EBPF_FILE_DESCRIPTION "eBPF Network Extension"
+#define EBPF_FILE_NAME "netebpfext.sys"
