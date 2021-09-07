498641217ac5f51141e597b88ae458be5812addf
Alan Jowett <alanjo@microsoft.com>
2021-09-07 11:22:00 -0600

    Cleanup parsing commitid
    Signed-off-by: Alan Jowett <alanjo@microsoft.com>

--- scripts/generate-commitid.ps1
+++ scripts/generate-commitid.ps1
@@ -6,11 +6,12 @@ param($OutDir)
 $file_name = ($OutDir + "\" + "git_commit_id.h")
 $commit_id = git rev-parse HEAD
 $new_content = '#define GIT_COMMIT_ID "' + $commit_id + '"' + "`n"
-[string]$old_content = ""
+[string]$old_commit_id = ""
 if (Test-Path $file_name) {
+    [string]$old_content = ""
     $old_content = Get-Content $file_name
+    $old_commit_id = $old_content.Split('"')[1]
 }
-$old_commit_id = $old_content.Split('"')[1]
 if ($old_commit_id -ne $commit_id) {
     Write-Output "Commit ID changed, regenerating $file_name"
     $new_content | out-file -FilePath $file_name
