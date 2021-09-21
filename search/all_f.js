bf5669475810015d576bf6bca3a848f87cbcd90c
Github Action <ebpf-for-windows@users.noreply.github.com>
2021-09-21 16:34:41 +0000

    Updated documentation
    Signed-off-by: Github Action <ebpf-for-windows@users.noreply.github.com>

--- search/all_f.js
+++ search/all_f.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['pid_5ft_280',['pid_t',['../linux_2bpf_8h.html#a7a26cb7f5678736cec66712d6df1d4ed',1,'bpf.h']]],
   ['pin_5fpath_281',['pin_path',['../struct__ebpf__map__info.html#ac63b0cf653937f3dd861cdd6f1e16721',1,'_ebpf_map_info']]],
-  ['pinned_5fpath_5fcount_282',['pinned_path_count',['../structbpf__prog__info.html#a3bf702e4104bc645d8873ad16f303f81',1,'bpf_prog_info']]],
+  ['pinned_5fpath_5fcount_282',['pinned_path_count',['../structbpf__map__info.html#ae6ca59a8016a681c1a2f41571ae21765',1,'bpf_map_info::pinned_path_count()'],['../structbpf__prog__info.html#a3bf702e4104bc645d8873ad16f303f81',1,'bpf_prog_info::pinned_path_count()']]],
   ['process_5fid_283',['process_id',['../struct__bind__md.html#a638d422f80edfb08491ba85bb3b1f97b',1,'_bind_md']]],
   ['prog_5fid_284',['prog_id',['../structbpf__link__info.html#a58b4021ef61000e376be819e1da615a6',1,'bpf_link_info']]],
   ['program_5finfo_285',['program_info',['../struct__ebpf__program__data.html#a32f4c9c6ad7e04e40a973ec6eb232a2b',1,'_ebpf_program_data']]],
--- search/variables_b.js
+++ search/variables_b.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['pin_5fpath_480',['pin_path',['../struct__ebpf__map__info.html#ac63b0cf653937f3dd861cdd6f1e16721',1,'_ebpf_map_info']]],
-  ['pinned_5fpath_5fcount_481',['pinned_path_count',['../structbpf__prog__info.html#a3bf702e4104bc645d8873ad16f303f81',1,'bpf_prog_info']]],
+  ['pinned_5fpath_5fcount_481',['pinned_path_count',['../structbpf__map__info.html#ae6ca59a8016a681c1a2f41571ae21765',1,'bpf_map_info::pinned_path_count()'],['../structbpf__prog__info.html#a3bf702e4104bc645d8873ad16f303f81',1,'bpf_prog_info::pinned_path_count()']]],
   ['process_5fid_482',['process_id',['../struct__bind__md.html#a638d422f80edfb08491ba85bb3b1f97b',1,'_bind_md']]],
   ['prog_5fid_483',['prog_id',['../structbpf__link__info.html#a58b4021ef61000e376be819e1da615a6',1,'bpf_link_info']]],
   ['program_5finfo_484',['program_info',['../struct__ebpf__program__data.html#a32f4c9c6ad7e04e40a973ec6eb232a2b',1,'_ebpf_program_data']]],
