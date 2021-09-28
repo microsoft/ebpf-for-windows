394f945162c2a4cc6f29c8e50c7693bfa7e98e79
Github Action <ebpf-for-windows@users.noreply.github.com>
2021-09-28 16:48:06 +0000

    Updated documentation
    Signed-off-by: Github Action <ebpf-for-windows@users.noreply.github.com>

--- search/enums_2.js
+++ search/enums_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['ebpf_5fhelper_5fid_5ft_581',['ebpf_helper_id_t',['../ebpf__structs_8h.html#aa752b12bfaedda8a28a7a88c1b96f272',1,'ebpf_structs.h']]],
-  ['ebpf_5fmap_5foption_582',['ebpf_map_option',['../ebpf__structs_8h.html#ad2aa050648c308e0be8329a2ade170fc',1,'ebpf_structs.h']]],
-  ['ebpf_5fresult_583',['ebpf_result',['../ebpf__result_8h.html#a916ce31f9038720787b441792c088372',1,'ebpf_result.h']]]
+  ['ebpf_5fhelper_5fid_5ft_585',['ebpf_helper_id_t',['../ebpf__structs_8h.html#aa752b12bfaedda8a28a7a88c1b96f272',1,'ebpf_structs.h']]],
+  ['ebpf_5fmap_5foption_586',['ebpf_map_option',['../ebpf__structs_8h.html#ad2aa050648c308e0be8329a2ade170fc',1,'ebpf_structs.h']]],
+  ['ebpf_5fresult_587',['ebpf_result',['../ebpf__result_8h.html#a916ce31f9038720787b441792c088372',1,'ebpf_result.h']]]
 ];
--- search/typedefs_0.js
+++ search/typedefs_0.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['_5f_5fbe16_529',['__be16',['../bpf__helpers__platform_8h.html#a96dbe1d4c138ab30ecf80a430fd5b307',1,'bpf_helpers_platform.h']]],
-  ['_5f_5fbe32_530',['__be32',['../bpf__helpers__platform_8h.html#a1fcbe0fe57dc78d034b654a327a171df',1,'bpf_helpers_platform.h']]],
-  ['_5f_5fs32_531',['__s32',['../bpf__helpers__platform_8h.html#a375730bf3d49b04b76cd45693a00e5d5',1,'__s32():&#160;bpf_helpers_platform.h'],['../linux_2bpf_8h.html#a375730bf3d49b04b76cd45693a00e5d5',1,'__s32():&#160;bpf.h']]],
-  ['_5f_5fs64_532',['__s64',['../bpf__helpers__platform_8h.html#a4e72ec36214b211237e947927a35b282',1,'bpf_helpers_platform.h']]],
-  ['_5f_5fu16_533',['__u16',['../bpf__helpers__platform_8h.html#abc70358187152575fd05fbf6d253151f',1,'bpf_helpers_platform.h']]],
-  ['_5f_5fu32_534',['__u32',['../bpf__helpers__platform_8h.html#a3acae9310e2c2e411e800a8a369171c6',1,'__u32():&#160;bpf_helpers_platform.h'],['../linux_2bpf_8h.html#a3acae9310e2c2e411e800a8a369171c6',1,'__u32():&#160;bpf.h']]],
-  ['_5f_5fu64_535',['__u64',['../bpf__helpers__platform_8h.html#ac2a2bfb32105ffe8e74b67670b7ec440',1,'__u64():&#160;bpf_helpers_platform.h'],['../linux_2bpf_8h.html#ac2a2bfb32105ffe8e74b67670b7ec440',1,'__u64():&#160;bpf.h']]],
-  ['_5f_5fu8_536',['__u8',['../linux_2bpf_8h.html#a77f8102529baef48621b98012524115d',1,'bpf.h']]],
-  ['_5f_5fwsum_537',['__wsum',['../bpf__helpers__platform_8h.html#ac491bd6047648581bd31da93cf180a4b',1,'bpf_helpers_platform.h']]]
+  ['_5f_5fbe16_533',['__be16',['../bpf__helpers__platform_8h.html#a96dbe1d4c138ab30ecf80a430fd5b307',1,'bpf_helpers_platform.h']]],
+  ['_5f_5fbe32_534',['__be32',['../bpf__helpers__platform_8h.html#a1fcbe0fe57dc78d034b654a327a171df',1,'bpf_helpers_platform.h']]],
+  ['_5f_5fs32_535',['__s32',['../bpf__helpers__platform_8h.html#a375730bf3d49b04b76cd45693a00e5d5',1,'__s32():&#160;bpf_helpers_platform.h'],['../linux_2bpf_8h.html#a375730bf3d49b04b76cd45693a00e5d5',1,'__s32():&#160;bpf.h']]],
+  ['_5f_5fs64_536',['__s64',['../bpf__helpers__platform_8h.html#a4e72ec36214b211237e947927a35b282',1,'bpf_helpers_platform.h']]],
+  ['_5f_5fu16_537',['__u16',['../bpf__helpers__platform_8h.html#abc70358187152575fd05fbf6d253151f',1,'bpf_helpers_platform.h']]],
+  ['_5f_5fu32_538',['__u32',['../bpf__helpers__platform_8h.html#a3acae9310e2c2e411e800a8a369171c6',1,'__u32():&#160;bpf_helpers_platform.h'],['../linux_2bpf_8h.html#a3acae9310e2c2e411e800a8a369171c6',1,'__u32():&#160;bpf.h']]],
+  ['_5f_5fu64_539',['__u64',['../bpf__helpers__platform_8h.html#ac2a2bfb32105ffe8e74b67670b7ec440',1,'__u64():&#160;bpf_helpers_platform.h'],['../linux_2bpf_8h.html#ac2a2bfb32105ffe8e74b67670b7ec440',1,'__u64():&#160;bpf.h']]],
+  ['_5f_5fu8_540',['__u8',['../linux_2bpf_8h.html#a77f8102529baef48621b98012524115d',1,'bpf.h']]],
+  ['_5f_5fwsum_541',['__wsum',['../bpf__helpers__platform_8h.html#ac491bd6047648581bd31da93cf180a4b',1,'bpf_helpers_platform.h']]]
 ];
