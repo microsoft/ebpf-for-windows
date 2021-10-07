c0339539fb85d8d6b4d0f3ff55408f9fc0d91e80
Shankar Seal <74580197+shankarseal@users.noreply.github.com>
2021-10-06 23:40:17 -0700

    selfhost runner (#622)
    * selfhost runner
    
    * Apply suggestions from code review
    
    * Remove Release from WF
    
    * Apply suggestions from code review
    
    Co-authored-by: Dave Thaler <dthaler@microsoft.com>
    
    Co-authored-by: Dave Thaler <dthaler@microsoft.com>

---
+++ scripts/cleanup_ebpf_cicd_tests.ps1
@@ -0,0 +1,27 @@
+﻿# Copyright (c) Microsoft Corporation
+# SPDX-License-Identifier: MIT
+
+param ([parameter(Mandatory=$false)][string] $Target = "TEST_VM",
+       [parameter(Mandatory=$false)][string] $LogFileName = "TestLog.log",
+       [parameter(Mandatory=$false)][string] $WorkingDirectory = $pwd.ToString(),
+       [parameter(Mandatory=$false)][string] $VMListJsonFileName = "vm_list.json")
+
+Push-Location $WorkingDirectory
+
+$TestVMCredential = Get-StoredCredential -Target $Target -ErrorAction Stop
+
+# Load other utility modules.
+Import-Module .\common.psm1 -Force -ArgumentList ($LogFileName) -WarningAction SilentlyContinue
+Import-Module .\config_test_vm.psm1 -Force -ArgumentList ($TestVMCredential.UserName, $TestVMCredential.Password, $WorkingDirectory, $LogFileName) -WarningAction SilentlyContinue
+
+# Read the config json.
+$Config = Get-Content ("{0}\{1}" -f $PSScriptRoot, $VMListJsonFileName) | ConvertFrom-Json
+$VMList = $Config.VMList
+
+# Import logs from VMs.
+Import-ResultsFromVM -VMList $VMList
+
+# Stop the VMs.
+Stop-AllVMs -VMList $VMList
+
+Pop-Location
