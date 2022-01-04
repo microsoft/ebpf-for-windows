43eeb22e6361819e9659f9cf38d57dc48c374677
Dave Thaler <dthaler@microsoft.com>
2022-01-04 10:02:31 -0800

    Add reference to catch2 issue
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

--- tests/libs/util/catch_wrapper.hpp
+++ tests/libs/util/catch_wrapper.hpp
@@ -8,6 +8,7 @@
 #pragma warning(disable : 26495) // Always initialize a member variable
 #pragma warning(disable : 26812) // Prefer 'enum class' over 'enum'
 #pragma warning(disable : 26816) // The pointer points to memory allocated on the stack
-#define CATCH_CONFIG_COLOUR_NONE 1
+
+#define CATCH_CONFIG_COLOUR_NONE 1 // Disable color until https://github.com/catchorg/Catch2/issues/2345 is fixed.
 #include "catch2\catch.hpp"
 #pragma warning(pop)
