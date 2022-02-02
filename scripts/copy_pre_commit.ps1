4a1e01ee08eeffb4196a270c1cd50cfb7beb03ff
saxena-anurag <43585259+saxena-anurag@users.noreply.github.com>
2022-02-02 12:58:00 -0800

    add script to copy pre-commit file to relative hooks folder (#745)
    Co-authored-by: Alan Jowett <alanjo@microsoft.com>

---
+++ scripts/copy_pre_commit.ps1
@@ -0,0 +1,14 @@
+# Copyright (c) Microsoft Corporation
+# SPDX-License-Identifier: MIT
+
+function copy-precommit
+{
+    param([string]$file_path)
+    $command = "git rev-parse --git-path hooks"
+    $destination = Invoke-Expression $command
+
+    Write-Host "Copy $file_path to $destination."
+    Copy-Item $file_path $destination
+}
+
+copy-precommit $args[0]
