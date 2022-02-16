7c1f80747fe43789832e5d6d1b6aefb449d48688
Alan Jowett <alanjo@microsoft.com>
2022-02-15 21:15:57 -0700

    PR feedback
    Signed-off-by: Alan Jowett <alanjo@microsoft.com>

--- tools/bpf2c/btf_parser.h
+++ tools/bpf2c/btf_parser.h
@@ -18,8 +18,7 @@ typedef struct _line_info
 typedef std::map<std::string, std::map<size_t, line_info_t>> btf_section_to_instruction_to_line_info_t;
 
 /**
- * @brief Parse a .btf and .btf.ext section from an ELF file and Map of section
- * name to map of offset to line information.
+ * @brief Parse a .btf and .btf.ext section from an ELF file.
  *
  * @param[in] btf The .btf section (containing type info and strings).
  * @param[in] btf_ext The .btf.ext section (containing function info and
