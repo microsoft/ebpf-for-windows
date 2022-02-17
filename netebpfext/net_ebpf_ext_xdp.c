6472d8d909f197c595f5e7668fba8e9936b1c6ce
Shankar Seal <sseal@microsoft.com>
2022-02-16 18:39:44 -0800

    fix crash.

--- netebpfext/net_ebpf_ext_xdp.c
+++ netebpfext/net_ebpf_ext_xdp.c
@@ -335,10 +335,12 @@ net_ebpf_ext_layer_2_classify(
     // the client data here.   In the future, the client data field should be
     // exposed in the .h file for us to access here.
     const ebpf_extension_data_t* client_data = _ebpf_ext_get_client_data(_ebpf_xdp_hook_provider_registration);
-    uint32_t client_ifindex = *(const uint32_t*)client_data->data;
-    if (client_ifindex != 0 && client_ifindex != net_xdp_ctx.ingress_ifindex) {
-        // The client is not interested in this ingress ifindex.
-        goto Done;
+    if (client_data->data != NULL) {
+        uint32_t client_ifindex = *(const uint32_t*)client_data->data;
+        if (client_ifindex != 0 && client_ifindex != net_xdp_ctx.ingress_ifindex) {
+            // The client is not interested in this ingress ifindex.
+            goto Done;
+        }
     }
 
     net_xdp_ctx.original_nbl = nbl;
