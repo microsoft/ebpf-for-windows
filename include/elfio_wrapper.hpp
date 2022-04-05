e0347312eda8d95a170762001f7bad0a57daee39
Dave Thaler <dthaler@microsoft.com>
2022-04-05 09:03:05 -0700

    Use upstream main branch for ebpf-verifier
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

--- include/elfio_wrapper.hpp
+++ include/elfio_wrapper.hpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: MIT
 
 #pragma warning(push)
+#pragma warning(disable : 4244)  // conversion from 'int' to 'ELFIO::Elf_Half', possible loss of data
 #pragma warning(disable : 4458)  // declaration of 'name' hides class member
 #pragma warning(disable : 6011)  // 'Dereferencing NULL pointer - https://github.com/vbpf/ebpf-verifier/issues/239
 #pragma warning(disable : 26451) // Arithmetic overflow
