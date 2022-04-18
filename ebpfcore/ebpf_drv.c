b01dea6d55cf31c38d9e464c4dc80ebae2396c93
Dave Thaler <dthaler@microsoft.com>
2022-04-18 09:26:29 -0700

    Fix reply id/length on failure in KM (#950)
    * Fix reply id/length on failure in KM
    
    And make UM and KM more closely follow the same logic to catch this sort
    of bug in the future.  Before this, UM would succeed and KM would fail,
    and there were only UM tests for this case.
    
    Fixes #946
    
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>
    
    * Fix device_helper.hpp
    
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>
    
    * Fix reply length
    
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>
    
    * Cleanup
    
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>
    
    * PR feedback
    
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>
    
    Co-authored-by: Alan Jowett <alanjo@microsoft.com>

--- ebpfcore/ebpf_drv.c
+++ ebpfcore/ebpf_drv.c
@@ -320,12 +320,6 @@ _ebpf_driver_io_device_control(
                     async_context,
                     _ebpf_driver_io_device_control_complete));
 
-                // Fill out the rest of the out buffer after processing the input
-                // buffer.
-                if (status == STATUS_SUCCESS && user_reply) {
-                    user_reply->id = user_request->id;
-                    user_reply->length = min((uint16_t)actual_output_length, user_reply->length);
-                }
                 goto Done;
             }
         } else {
