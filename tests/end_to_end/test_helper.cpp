1acb4d631ee6cd3c012fa3a193e7e604b49f20ba
Alan Jowett <alan.jowett@microsoft.com>
2022-04-21 20:53:00 -0600

    Add script to simplify generating native images from BPF.
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

--- tests/end_to_end/test_helper.cpp
+++ tests/end_to_end/test_helper.cpp
@@ -241,29 +241,14 @@ _unload_all_native_modules()
 static void
 _preprocess_load_native_module(_Inout_ service_context_t* context)
 {
-    // For user mode tests, most of the sample programs are compiled into a single DLL file.
-    // First try loading from there.
-    context->dll = LoadLibraryA(NATIVE_DLL_NAME);
+    context->dll = LoadLibraryW(context->file_path.c_str());
     REQUIRE(context->dll != nullptr);
 
-    // Get file name from the file path.
-    std::string file_name = std::filesystem::path(context->file_path).filename().stem().string();
+    auto get_function =
+        reinterpret_cast<decltype(&get_metadata_table)>(GetProcAddress(context->dll, "get_metadata_table"));
+    REQUIRE(get_function != nullptr);
 
-    auto get_function_common =
-        reinterpret_cast<decltype(&get_metadata_table_common)>(GetProcAddress(context->dll, "get_metadata_table"));
-    REQUIRE(get_function_common != nullptr);
-    metadata_table_t* table = get_function_common(file_name.c_str());
-    if (table == nullptr) {
-        // Possibly this sample program in not in the common DLL file. Try loading the actual DLL.
-        FreeLibrary(context->dll);
-        context->dll = LoadLibraryW(context->file_path.c_str());
-        REQUIRE(context->dll != nullptr);
-
-        auto get_function =
-            reinterpret_cast<decltype(&get_metadata_table)>(GetProcAddress(context->dll, "get_metadata_table"));
-        REQUIRE(get_function != nullptr);
-        table = get_function();
-    }
+    metadata_table_t* table = get_function();
     REQUIRE(table != nullptr);
 
     int client_binding_context = 0;
