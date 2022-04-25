2cdadc91319448314d4b159f3eff592b388f76e8
Alan Jowett <alanjo@microsoft.com>
2022-04-25 12:29:10 -0600

    Force compiler to generate singleton for WER
    Signed-off-by: Alan Jowett <alanjo@microsoft.com>

--- libs/platform/unit/platform_unit_test.cpp
+++ libs/platform/unit/platform_unit_test.cpp
@@ -834,9 +834,5 @@ TEST_CASE("error codes", "[platform]")
         REQUIRE(result2 == result);
     }
 }
-extern bool _wer_report_enabled;
-TEST_CASE("Generate dump", "[platform]")
-{
-    REQUIRE(_wer_report_enabled);
-    RaiseException(EXCEPTION_ACCESS_VIOLATION, 0, 0, 0);
-}
+
+TEST_CASE("Generate dump", "[platform]") { RaiseException(EXCEPTION_ACCESS_VIOLATION, 0, 0, 0); }
