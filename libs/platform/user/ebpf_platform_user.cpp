10f8cb7ab9d4d0f30c402cd2f1fe6e880d186aa8
Anurag Saxena <43585259+saxena-anurag@users.noreply.github.com>
2022-04-29 21:17:08 -0700

    Revert "workaround (#1040)"
    This reverts commit 24f098b8690c9d51f1c154d9976faeac19dc99f5.

--- libs/platform/user/ebpf_platform_user.cpp
+++ libs/platform/user/ebpf_platform_user.cpp
@@ -428,7 +428,9 @@ ebpf_lock_destroy(_In_ ebpf_lock_t* lock)
 }
 
 _Requires_lock_not_held_(*lock) _Acquires_lock_(*lock) _IRQL_requires_max_(DISPATCH_LEVEL) _IRQL_saves_
-    _IRQL_raises_(DISPATCH_LEVEL) ebpf_lock_state_t ebpf_lock_lock(_In_ ebpf_lock_t* lock)
+    _IRQL_raises_(DISPATCH_LEVEL)
+ebpf_lock_state_t
+ebpf_lock_lock(_In_ ebpf_lock_t* lock)
 {
     AcquireSRWLockExclusive(reinterpret_cast<PSRWLOCK>(lock));
     return 0;
@@ -510,7 +512,7 @@ ebpf_is_non_preemptible_work_item_supported()
 bool
 ebpf_is_preemptible_work_item_supported()
 {
-    return false;
+    return true;
 }
 
 uint32_t
