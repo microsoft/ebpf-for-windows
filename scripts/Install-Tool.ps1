f7c98301526b1d3a57add0ac9f9c76025b3e77d8
Alan Jowett <alan.jowett@microsoft.com>
2022-05-11 20:49:33 -0600

    Debug install of everparse
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

--- scripts/Install-Tool.ps1
+++ scripts/Install-Tool.ps1
@@ -9,19 +9,25 @@ param ([parameter(Mandatory = $true)][uri] $URL,
 
 $FileName = $Url.Segments[$Url.Segments.Count - 1]
 
+Write-Host "Creating folder " $PackageRoot\downloads
 mkdir -path $PackageRoot\downloads -ErrorAction SilentlyContinue
 
+Write-Host "Checking for " $PackageRoot\downloads\$FileName
 $FileExists = Test-Path $PackageRoot\downloads\$FileName
 if ($FileExists -ne $true) {
+    Write-Host "Downloading file " $URL
     Invoke-WebRequest -Uri $URL -OutFile $PackageRoot\downloads\$FileName -ErrorAction SilentlyContinue
 }
 
+Write-Host "Checking hash of file " $PackageRoot\downloads\$FileName
 $DownloadHash = (Get-FileHash -Path $PackageRoot\downloads\$FileName).Hash
 if ($DownloadHash -ne $PackageHash) {
     throw "Downloaded copy of " + $PackageName + " hash wrong hash"
 }
 
+Write-Host "Checking tool folder " $PackageRoot\$PackageName
 $PackageInstalled = Test-Path $PackageRoot\$PackageName
 if ($PackageInstalled -ne $true) {
+    Write-Host "Expanding file " $PackageRoot\downloads\$FileName
     Expand-Archive -Path $PackageRoot\downloads\$FileName -DestinationPath $PackageRoot
 }
