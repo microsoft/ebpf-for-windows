c31f6ca253edd29e5d2edbd38d0f3ad493d5b9dd
song jiang <song@tigera.io>
2022-05-23 14:23:04 +0100

    Added Powershell script to build image on Windows node

---
+++ images/build-images.ps1
@@ -0,0 +1,19 @@
+# Copyright (c) Microsoft Corporation
+# SPDX-License-Identifier: MIT
+
+param ([parameter(Mandatory=$false)][string] $TEMPDir = "c:\temp",
+    [parameter(Mandatory=$true)][string] $Repositry = "",
+    [parameter(Mandatory=$true)][string] $Tag = "",
+    [parameter(Mandatory=$true)][string] $OSVersion = "1809")
+
+$svc = Get-Service | where Name -EQ 'docker'
+if ($svc -EQ $null) {
+    throw "Docker service is not installed."
+}
+if ($svc.Status -NE 'Running') {
+    throw "Docker service is not running."
+}
+
+Compress-Archive -Update -Path $TEMPDir -DestinationPath ebpf-for-windows-c-temp.zip
+
+docker build -t $Repositry/ebpfwin-install:$Tag  -f .\Dockerfile.install --build-arg WINDOWS_VERSION=$OSVersion .
