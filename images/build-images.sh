52dbddadab5ab0090e040586964428d3b172916b
song jiang <song@tigera.io>
2022-05-23 16:54:09 +0100

    Minor fix

--- images/build-images.sh
+++ images/build-images.sh
@@ -5,5 +5,6 @@
 
 repository=${repository:-"your repository"}
 tag=${tag:-"your tag"}
+OSVersion=${OSVersion:-"1809"}
 
-docker buildx build --platform windows/amd64 --output=type=registry --pull -f Dockerfile.install -t $repository/ebpfwin-install:$tag .
+docker buildx build --platform windows/amd64 --output=type=registry --pull -f Dockerfile.install -t $repository/ebpfwin-install:$tag --build-arg WINDOWS_VERSION=$OSVersion .
