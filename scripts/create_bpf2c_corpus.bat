46135e4a507d4ae016587f9072e5f4f9b94d2014
Dave Thaler <dthaler@microsoft.com>
2022-05-31 15:10:17 -0700

    Temporarily revert more files
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

--- scripts/create_bpf2c_corpus.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem Copyright (c) Microsoft Corporation
-rem SPDX-License-Identifier: MIT
-rem
-rem Usage: create_bpf2c_corpus.bat <output_directory>
-
-set OUTPUTPATH=%1
-xcopy /d /i /y "%OUTPUTPATH%\*.o" "%OUTPUTPATH%\bpf2c_fuzzer_corpus"
--- scripts/create_core_helper_corpus.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem Copyright (c) Microsoft Corporation
-rem SPDX-License-Identifier: MIT
-rem
-rem Usage: create_core_helper_corpus.bat <solution_path> <output_directory>
-
-set SOLUTIONPATH=%1
-set OUTPUTPATH=%2
-xcopy /d /i /y "%SOLUTIONPATH%\tests\libfuzzer\core_helper_fuzzer\corpus" "%OUTPUTPATH%"
