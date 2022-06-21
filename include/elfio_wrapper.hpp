1fea5bb8cad9f87939f1f74a4f274aefe3ed610e
Dave Thaler <dthaler@microsoft.com>
2022-06-21 16:32:19 -0700

    Update to latest verifier code (#1235)
    * Update to latest verifier code
    
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>
    
    * Suppress elfio warning
    
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

--- include/elfio_wrapper.hpp
+++ include/elfio_wrapper.hpp
@@ -3,6 +3,7 @@
 
 #pragma warning(push)
 #pragma warning(disable : 4244)  // conversion from 'int' to 'ELFIO::Elf_Half', possible loss of data
+#pragma warning(disable : 4389)  // signed/unsigned mismatch
 #pragma warning(disable : 4458)  // declaration of 'name' hides class member
 #pragma warning(disable : 6011)  // 'Dereferencing NULL pointer - https://github.com/vbpf/ebpf-verifier/issues/239
 #pragma warning(disable : 26451) // Arithmetic overflow
