ea49b0ba92e2ddf8dd0c5e650c5adf4857bdd6b4
Shankar Seal <74580197+shankarseal@users.noreply.github.com>
2022-06-29 16:28:04 -0700

    Fix test automation. (#1258)
    * Fix test automation.
    
    * fix.

--- scripts/config_test_vm.psm1
+++ scripts/config_test_vm.psm1
@@ -256,8 +256,19 @@ function Import-ResultsFromVM
         Write-Log ("Copy CodeCoverage from eBPF on $VMName to $pwd\..\..")
         Copy-Item -FromSession $VMSession "$VMSystemDrive\eBPF\ebpf_for_windows.xml" -Destination "$pwd\..\.." -Recurse -Force -ErrorAction Ignore 2>&1 | Write-Log
 
-        # Copy ETL from Test VM.
         $EtlFile = $LogFileName.Substring(0, $LogFileName.IndexOf('.')) + ".etl"
+
+        # Stop ETW Traces.
+        Invoke-Command -Session $VMSession -ScriptBlock {
+            param([Parameter(Mandatory=$True)] [string] $LogFileName,
+                  [Parameter(Mandatory=$True)] [string] $EtlFile)
+            Import-Module $WorkingDirectory\common.psm1 -ArgumentList ($LogFileName) -Force -WarningAction SilentlyContinue
+
+            Write-Log ("Stopping ETW tracing, creating file: " + $EtlFile)
+            Start-Process -FilePath "wpr.exe" -ArgumentList @("-stop", $EtlFile) -NoNewWindow -Wait
+        } -ArgumentList ($LogFileName, $EtlFile) -ErrorAction Ignore
+
+        # Copy ETL from Test VM.
         Write-Log ("Copy $EtlFile from eBPF on $VMName to $pwd\TestLogs")
         Copy-Item -FromSession $VMSession -Path "$VMSystemDrive\eBPF\$EtlFile" -Destination ".\TestLogs\$VMName\Logs" -Recurse -Force -ErrorAction Ignore 2>&1 | Write-Log
     }
