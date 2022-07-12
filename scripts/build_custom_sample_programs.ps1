5dbdb085e70003d4fce2dcc180bb1cdf3ea825e8
Anurag Saxena <anusa@microsoft.com>
2022-07-12 01:04:35 -0700

    add --type option to bpf2c tool

---
+++ scripts/build_custom_sample_programs.ps1
@@ -0,0 +1,23 @@
+# Copyright (c) Microsoft Corporation
+# SPDX-License-Identifier: MIT
+
+param([Parameter(Mandatory=$True)][string]$FileName,
+      [Parameter(Mandatory=$True)][string]$FilePath,
+      [Parameter(Mandatory=$True)][string]$Platform,
+      [Parameter(Mandatory=$True)][string]$Configuration,
+      [Parameter(Mandatory=$True)][string]$IncludePath)
+
+Push-Location $FilePath
+
+$ProgramType = ""
+
+if ($FileName -eq "bpf")
+{
+    $ProgramType = "xdp"
+}
+
+.\Convert-BpfToNative.ps1 -ProgramName $Filename -Type $ProgramType -IncludeDir $IncludePath -Platform $Platform -Configuration $Configuration -KernelMode $True
+.\Convert-BpfToNative.ps1 -ProgramName $Filename -Type $ProgramType -IncludeDir $IncludePath -Platform $Platform -Configuration $Configuration -KernelMode $False
+
+
+Pop-Location
