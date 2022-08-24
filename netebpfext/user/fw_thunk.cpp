45840d3c7921d8afb16e02dccb406386f21f80c2
Dave Thaler <dthaler@microsoft.com>
2022-08-24 07:58:44 -0700

    Clean up fw_thunk.h (#1369)
    * Clean up fw_thunk.h
    
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>
    
    * Fix SAL annotations
    
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>
    
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

--- netebpfext/user/fw_thunk.cpp
+++ netebpfext/user/fw_thunk.cpp
@@ -4,24 +4,22 @@
 #include "netebpfext_platform.h"
 #include "fw_thunk.h"
 
-NTSTATUS
-FwpmFilterDeleteById0(_In_ HANDLE engine_handle, _In_ uint64_t id)
+_IRQL_requires_max_(PASSIVE_LEVEL) NTSTATUS FwpmFilterDeleteById0(_In_ HANDLE engine_handle, _In_ uint64_t id)
 {
     UNREFERENCED_PARAMETER(engine_handle);
     UNREFERENCED_PARAMETER(id);
     return STATUS_NOT_IMPLEMENTED;
 }
 
-NTSTATUS
-FwpmTransactionBegin0(_In_ _Acquires_lock_(_Curr_) HANDLE engine_handle, _In_ uint32_t flags)
+_IRQL_requires_max_(PASSIVE_LEVEL) NTSTATUS
+    FwpmTransactionBegin0(_In_ _Acquires_lock_(_Curr_) HANDLE engine_handle, _In_ uint32_t flags)
 {
     UNREFERENCED_PARAMETER(engine_handle);
     UNREFERENCED_PARAMETER(flags);
     return STATUS_NOT_IMPLEMENTED;
 }
 
-NTSTATUS
-FwpmFilterAdd0(
+_IRQL_requires_max_(PASSIVE_LEVEL) NTSTATUS FwpmFilterAdd0(
     _In_ HANDLE engine_handle,
     _In_ const FWPM_FILTER0* filter,
     _In_opt_ PSECURITY_DESCRIPTOR sd,
@@ -34,22 +32,20 @@ FwpmFilterAdd0(
     return STATUS_NOT_IMPLEMENTED;
 }
 
-NTSTATUS
-FwpmTransactionCommit0(_In_ _Releases_lock_(_Curr_) HANDLE engine_handle)
+_IRQL_requires_max_(PASSIVE_LEVEL) NTSTATUS FwpmTransactionCommit0(_In_ _Releases_lock_(_Curr_) HANDLE engine_handle)
 {
     UNREFERENCED_PARAMETER(engine_handle);
     return STATUS_NOT_IMPLEMENTED;
 }
 
-NTSTATUS
-FwpmTransactionAbort0(_In_ _Releases_lock_(_Curr_) HANDLE engine_handle)
+_IRQL_requires_max_(PASSIVE_LEVEL) NTSTATUS FwpmTransactionAbort0(_In_ _Releases_lock_(_Curr_) HANDLE engine_handle)
 {
     UNREFERENCED_PARAMETER(engine_handle);
     return STATUS_NOT_IMPLEMENTED;
 }
 
-NTSTATUS
-FwpsCalloutRegister3(_Inout_ void* device_object, _In_ const FWPS_CALLOUT3* callout, _Out_opt_ uint32_t* callout_id)
+_IRQL_requires_max_(PASSIVE_LEVEL) NTSTATUS
+    FwpsCalloutRegister3(_Inout_ void* device_object, _In_ const FWPS_CALLOUT3* callout, _Out_opt_ uint32_t* callout_id)
 {
     UNREFERENCED_PARAMETER(device_object);
     UNREFERENCED_PARAMETER(callout);
@@ -57,8 +53,7 @@ FwpsCalloutRegister3(_Inout_ void* device_object, _In_ const FWPS_CALLOUT3* call
     return STATUS_NOT_IMPLEMENTED;
 }
 
-NTSTATUS
-FwpmCalloutAdd0(
+_IRQL_requires_max_(PASSIVE_LEVEL) NTSTATUS FwpmCalloutAdd0(
     _In_ HANDLE engine_handle,
     _In_ const FWPM_CALLOUT0* callout,
     _In_opt_ PSECURITY_DESCRIPTOR sd,
@@ -71,15 +66,13 @@ FwpmCalloutAdd0(
     return STATUS_NOT_IMPLEMENTED;
 }
 
-NTSTATUS
-FwpsCalloutUnregisterById0(_In_ const uint32_t callout_id)
+_IRQL_requires_max_(PASSIVE_LEVEL) NTSTATUS FwpsCalloutUnregisterById0(_In_ const uint32_t callout_id)
 {
     UNREFERENCED_PARAMETER(callout_id);
     return STATUS_NOT_IMPLEMENTED;
 }
 
-NTSTATUS
-FwpmEngineOpen0(
+_IRQL_requires_max_(PASSIVE_LEVEL) NTSTATUS FwpmEngineOpen0(
     _In_opt_ const wchar_t* server_name,
     _In_ uint32_t authn_service,
     _In_opt_ SEC_WINNT_AUTH_IDENTITY_W* auth_identity,
@@ -94,8 +87,8 @@ FwpmEngineOpen0(
     return STATUS_NOT_IMPLEMENTED;
 }
 
-NTSTATUS
-FwpmSubLayerAdd0(_In_ HANDLE engine_handle, _In_ const FWPM_SUBLAYER0* sub_layer, _In_opt_ PSECURITY_DESCRIPTOR sd)
+_IRQL_requires_max_(PASSIVE_LEVEL) NTSTATUS
+    FwpmSubLayerAdd0(_In_ HANDLE engine_handle, _In_ const FWPM_SUBLAYER0* sub_layer, _In_opt_ PSECURITY_DESCRIPTOR sd)
 {
     UNREFERENCED_PARAMETER(engine_handle);
     UNREFERENCED_PARAMETER(sub_layer);
@@ -103,8 +96,8 @@ FwpmSubLayerAdd0(_In_ HANDLE engine_handle, _In_ const FWPM_SUBLAYER0* sub_layer
     return STATUS_NOT_IMPLEMENTED;
 }
 
-NTSTATUS
-FwpsInjectionHandleCreate0(_In_opt_ ADDRESS_FAMILY address_family, _In_ uint32_t flags, _Out_ HANDLE* injection_handle)
+_IRQL_requires_max_(PASSIVE_LEVEL) NTSTATUS FwpsInjectionHandleCreate0(
+    _In_opt_ ADDRESS_FAMILY address_family, _In_ uint32_t flags, _Out_ HANDLE* injection_handle)
 {
     UNREFERENCED_PARAMETER(address_family);
     UNREFERENCED_PARAMETER(flags);
@@ -112,22 +105,20 @@ FwpsInjectionHandleCreate0(_In_opt_ ADDRESS_FAMILY address_family, _In_ uint32_t
     return STATUS_NOT_IMPLEMENTED;
 }
 
-NTSTATUS
-FwpmEngineClose0(_Inout_ HANDLE engine_handle)
+_IRQL_requires_max_(PASSIVE_LEVEL) NTSTATUS FwpmEngineClose0(_Inout_ HANDLE engine_handle)
 {
     UNREFERENCED_PARAMETER(engine_handle);
     return STATUS_NOT_IMPLEMENTED;
 }
 
-NTSTATUS
-FwpsInjectionHandleDestroy0(_In_ HANDLE injection_handle)
+_IRQL_requires_max_(PASSIVE_LEVEL) NTSTATUS FwpsInjectionHandleDestroy0(_In_ HANDLE injection_handle)
 {
     UNREFERENCED_PARAMETER(injection_handle);
     return STATUS_NOT_IMPLEMENTED;
 }
 
-NTSTATUS
-FwpsFlowRemoveContext0(_In_ uint64_t flow_id, _In_ UINT16 layer_id, _In_ uint32_t callout_id)
+_IRQL_requires_max_(DISPATCH_LEVEL) NTSTATUS
+    FwpsFlowRemoveContext0(_In_ uint64_t flow_id, _In_ UINT16 layer_id, _In_ uint32_t callout_id)
 {
     UNREFERENCED_PARAMETER(flow_id);
     UNREFERENCED_PARAMETER(layer_id);
@@ -135,8 +126,7 @@ FwpsFlowRemoveContext0(_In_ uint64_t flow_id, _In_ UINT16 layer_id, _In_ uint32_
     return STATUS_NOT_IMPLEMENTED;
 }
 
-NTSTATUS
-FwpsFlowAssociateContext0(
+_IRQL_requires_max_(DISPATCH_LEVEL) NTSTATUS FwpsFlowAssociateContext0(
     _In_ uint64_t flow_id, _In_ UINT16 layer_id, _In_ uint32_t callout_id, _In_ uint64_t flowContext)
 {
     UNREFERENCED_PARAMETER(flow_id);
@@ -146,8 +136,7 @@ FwpsFlowAssociateContext0(
     return STATUS_NOT_IMPLEMENTED;
 }
 
-NTSTATUS
-FwpsAllocateNetBufferAndNetBufferList0(
+_IRQL_requires_max_(DISPATCH_LEVEL) NTSTATUS FwpsAllocateNetBufferAndNetBufferList0(
     _In_ NDIS_HANDLE pool_handle,
     _In_ uint16_t context_size,
     _In_ uint16_t context_backfill,
@@ -166,8 +155,7 @@ FwpsAllocateNetBufferAndNetBufferList0(
     return STATUS_NOT_IMPLEMENTED;
 }
 
-void
-FwpsFreeNetBufferList0(_In_ NET_BUFFER_LIST* net_buffer_list)
+_IRQL_requires_max_(DISPATCH_LEVEL) void FwpsFreeNetBufferList0(_In_ NET_BUFFER_LIST* net_buffer_list)
 {
     UNREFERENCED_PARAMETER(net_buffer_list);
 }
@@ -196,15 +184,14 @@ FwpsInjectMacReceiveAsync0(
     return STATUS_NOT_IMPLEMENTED;
 }
 
-void
-FwpsFreeCloneNetBufferList0(_In_ NET_BUFFER_LIST* net_buffer_list, _In_ unsigned long free_clone_flags)
+_IRQL_requires_max_(DISPATCH_LEVEL) void FwpsFreeCloneNetBufferList0(
+    _In_ NET_BUFFER_LIST* net_buffer_list, _In_ unsigned long free_clone_flags)
 {
     UNREFERENCED_PARAMETER(net_buffer_list);
     UNREFERENCED_PARAMETER(free_clone_flags);
 }
 
-NTSTATUS
-FwpsAllocateCloneNetBufferList0(
+_IRQL_requires_max_(DISPATCH_LEVEL) NTSTATUS FwpsAllocateCloneNetBufferList0(
     _Inout_ NET_BUFFER_LIST* original_net_buffer_list,
     _In_opt_ NDIS_HANDLE net_buffer_list_pool_handle,
     _In_opt_ NDIS_HANDLE net_buffer_pool_handle,
@@ -220,17 +207,17 @@ FwpsAllocateCloneNetBufferList0(
     return STATUS_NOT_IMPLEMENTED;
 }
 
-NTSTATUS
-FwpsInjectMacSendAsync0(
-    _In_ HANDLE injection_handle,
-    _In_opt_ HANDLE injection_context,
-    _In_ uint32_t flags,
-    _In_ UINT16 layer_id,
-    _In_ IF_INDEX interface_index,
-    _In_ NDIS_PORT_NUMBER ndis_port_number,
-    _Inout_ NET_BUFFER_LIST* net_buffer_lists,
-    _In_ void* completion_function,
-    _In_opt_ HANDLE completion_context)
+_IRQL_requires_min_(PASSIVE_LEVEL) _IRQL_requires_max_(DISPATCH_LEVEL) _Must_inspect_result_ NTSTATUS
+    FwpsInjectMacSendAsync0(
+        _In_ HANDLE injection_handle,
+        _In_opt_ HANDLE injection_context,
+        _In_ uint32_t flags,
+        _In_ UINT16 layer_id,
+        _In_ IF_INDEX interface_index,
+        _In_ NDIS_PORT_NUMBER ndis_port_number,
+        _Inout_ NET_BUFFER_LIST* net_buffer_lists,
+        _In_ void* completion_function,
+        _In_opt_ HANDLE completion_context)
 {
     UNREFERENCED_PARAMETER(injection_handle);
     UNREFERENCED_PARAMETER(injection_context);
