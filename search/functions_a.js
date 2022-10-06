2d21dfe7cf44a990794e7e7039c6327b3ade375e
Github Action <ebpf-for-windows@users.noreply.github.com>
2022-10-06 20:49:07 +0000

    Updated documentation
    Signed-off-by: Github Action <ebpf-for-windows@users.noreply.github.com>

--- search/functions_a.js
+++ search/functions_a.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['win32_5ferror_5fcode_5fto_5febpf_5fresult_944',['win32_error_code_to_ebpf_result',['../ebpf__utilities_8h.html#a7d83d74a974c4fd180f638ca9b28bf9f',1,'ebpf_utilities.h']]],
-  ['write_5fregistry_5fvalue_5fansi_5fstring_945',['write_registry_value_ansi_string',['../user_2ebpf__registry__helper_8h.html#a8aa349ada77d337b528e407fcf4c226e',1,'ebpf_registry_helper.h']]],
-  ['write_5fregistry_5fvalue_5fbinary_946',['write_registry_value_binary',['../user_2ebpf__registry__helper_8h.html#a8ac9b1a630e07ab18dd1e14f4996f818',1,'ebpf_registry_helper.h']]],
-  ['write_5fregistry_5fvalue_5fdword_947',['write_registry_value_dword',['../user_2ebpf__registry__helper_8h.html#a70ec7616cadc98c033f052354316f730',1,'ebpf_registry_helper.h']]]
+  ['win32_5ferror_5fcode_5fto_5febpf_5fresult_942',['win32_error_code_to_ebpf_result',['../ebpf__utilities_8h.html#a7d83d74a974c4fd180f638ca9b28bf9f',1,'ebpf_utilities.h']]],
+  ['write_5fregistry_5fvalue_5fansi_5fstring_943',['write_registry_value_ansi_string',['../user_2ebpf__registry__helper_8h.html#a8aa349ada77d337b528e407fcf4c226e',1,'ebpf_registry_helper.h']]],
+  ['write_5fregistry_5fvalue_5fbinary_944',['write_registry_value_binary',['../user_2ebpf__registry__helper_8h.html#a8ac9b1a630e07ab18dd1e14f4996f818',1,'ebpf_registry_helper.h']]],
+  ['write_5fregistry_5fvalue_5fdword_945',['write_registry_value_dword',['../user_2ebpf__registry__helper_8h.html#a70ec7616cadc98c033f052354316f730',1,'ebpf_registry_helper.h']]]
 ];
