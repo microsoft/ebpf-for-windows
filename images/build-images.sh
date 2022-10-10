561fd1dfd9fbcbaf3c156ebecc96c4dc54d4e2d4
Song Jiang <song@tigera.io>
2022-10-10 20:21:42 +0000

    Pin docker base image with hash (#1447)
    * Pin docker base image with hash
    
    * Review Markups
    
    Co-authored-by: Dave Thaler <dthaler@microsoft.com>
    Co-authored-by: Alan Jowett <alanjo@microsoft.com>

--- images/build-images.sh
+++ images/build-images.sh
@@ -5,6 +5,5 @@
 
 repository=${repository:-"your repository"}
 tag=${tag:-"your tag"}
-OSVersion=${OSVersion:-"1809"}
 
-docker buildx build --platform windows/amd64 --output=type=registry --pull -f Dockerfile.install -t $repository/ebpfwin-install:$tag --build-arg WINDOWS_VERSION=$OSVersion .
+docker buildx build --platform windows/amd64 --output=type=registry --pull -f Dockerfile.install -t $repository/ebpfwin-install:$tag .
