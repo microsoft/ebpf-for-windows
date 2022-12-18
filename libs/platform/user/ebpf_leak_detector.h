06d01150a8c7bd9f5810cdd82b52e67823dbeced
Alan Jowett <alanjo@microsoft.com>
2022-12-18 11:47:17 -0700

    Add support for logging leaked allocations
    Signed-off-by: Alan Jowett <alanjo@microsoft.com>

---
+++ libs/platform/user/ebpf_leak_detector.h
@@ -0,0 +1,39 @@
+// Copyright (c) Microsoft Corporation
+// SPDX-License-Identifier: MIT
+#pragma once
+#include <unordered_map>
+#include <vector>
+#include <mutex>
+
+#include "ebpf_platform.h"
+
+typedef class _ebpf_leak_detector
+{
+  public:
+    _ebpf_leak_detector() = default;
+    ~_ebpf_leak_detector() = default;
+
+    void
+    register_allocation(uintptr_t address, size_t size);
+
+    void
+    unregister_allocation(uintptr_t address);
+
+    void
+    dump_leaks();
+
+  private:
+    typedef struct _allocation
+    {
+        uintptr_t address;
+        size_t size;
+        unsigned long stack_hash;
+    } allocation_t;
+
+    std::unordered_map<unsigned long, std::vector<uintptr_t>> _stack_hashes;
+    std::unordered_map<uintptr_t, allocation_t> _allocations;
+    std::mutex _mutex;
+    const unsigned long _stack_depth = 32;
+} ebpf_leak_detector_t;
+
+typedef std::unique_ptr<ebpf_leak_detector_t> ebpf_leak_detector_ptr;
