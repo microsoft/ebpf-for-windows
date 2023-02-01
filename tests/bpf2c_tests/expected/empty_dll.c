418f2fa5484b1a1c9728de615f6e5978d5bf25f9
Alan Jowett <alanjo@microsoft.com>
2023-01-31 17:52:37 -0700

    Fix test failures
    Signed-off-by: Alan Jowett <alanjo@microsoft.com>

--- tests/bpf2c_tests/expected/empty_dll.c
+++ tests/bpf2c_tests/expected/empty_dll.c
@@ -85,4 +85,4 @@ _get_version(_Out_ bpf2c_version_t* version)
     version->revision = 0;
 }
 
-metadata_table_t empty_metadata_table = {_get_programs, _get_maps, _get_hash, _get_version};
+metadata_table_t empty_metadata_table = {sizeof(metadata_table_t), _get_programs, _get_maps, _get_hash, _get_version};
