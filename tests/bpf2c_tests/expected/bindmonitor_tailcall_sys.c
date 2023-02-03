f50dcb999d30312d0c9156c6385f7206a6a22f29
Dhiren Vispute <86131363+dv-msft@users.noreply.github.com>
2023-02-03 09:33:18 -0800

    Use weak reference counts for object table entries (#2004)
    Co-authored-by: Alan Jowett <alanjo@microsoft.com>

--- tests/bpf2c_tests/expected/bindmonitor_tailcall_sys.c
+++ tests/bpf2c_tests/expected/bindmonitor_tailcall_sys.c
@@ -204,7 +204,7 @@ static map_entry_t _maps[] = {
          BPF_MAP_TYPE_PROG_ARRAY, // Type of map.
          4,                       // Size in bytes of a map key.
          4,                       // Size in bytes of a map value.
-         2,                       // Maximum number of entries allowed in the map.
+         8,                       // Maximum number of entries allowed in the map.
          0,                       // Inner map index.
          PIN_NONE,                // Pinning type for the map.
          0,                       // Identifier for a map template.
