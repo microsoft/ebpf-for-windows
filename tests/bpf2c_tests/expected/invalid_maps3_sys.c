c496a7135e06f1a57f2241079be54a85a8f2ca6a
Alan Jowett <alanjo@microsoft.com>
2023-02-09 10:26:28 -0700

    Update scripts\generate_expected_bpf2c_output.ps1 to generate custom and unsafe programs
    Signed-off-by: Alan Jowett <alanjo@microsoft.com>

--- tests/bpf2c_tests/expected/invalid_maps3_sys.c
+++ tests/bpf2c_tests/expected/invalid_maps3_sys.c
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 
 // Do not alter this generated file.
-// This file was generated from bpf.o
+// This file was generated from invalid_maps3.o
 
 #define NO_CRT
 #include "bpf2c.h"
@@ -15,7 +15,7 @@ DRIVER_INITIALIZE DriverEntry;
 DRIVER_UNLOAD DriverUnload;
 RTL_QUERY_REGISTRY_ROUTINE static _bpf2c_query_registry_routine;
 
-#define metadata_table bpf##_metadata_table
+#define metadata_table invalid_maps3##_metadata_table
 
 static GUID _bpf2c_npi_id = {/* c847aac8-a6f2-4b53-aea3-f4a94b9a80cb */
                              0xc847aac8,
@@ -173,43 +173,62 @@ _get_hash(_Outptr_result_buffer_maybenull_(*size) const uint8_t** hash, _Out_ si
     *hash = NULL;
     *size = 0;
 }
+#pragma data_seg(push, "maps")
+static map_entry_t _maps[] = {
+    {NULL,
+     {
+         BPF_MAP_TYPE_HASH, // Type of map.
+         8,                 // Size in bytes of a map key.
+         4,                 // Size in bytes of a map value.
+         1024,              // Maximum number of entries allowed in the map.
+         0,                 // Inner map index.
+         10,                // Pinning type for the map.
+         0,                 // Identifier for a map template.
+         0,                 // The id of the inner map template.
+     },
+     "process_map"},
+};
+#pragma data_seg(pop)
+
 static void
 _get_maps(_Outptr_result_buffer_maybenull_(*count) map_entry_t** maps, _Out_ size_t* count)
 {
-    *maps = NULL;
-    *count = 0;
+    *maps = _maps;
+    *count = 1;
 }
 
-static GUID func_program_type_guid = {0xf1832a85, 0x85d5, 0x45b0, {0x98, 0xa0, 0x70, 0x69, 0xd6, 0x30, 0x13, 0xb0}};
-static GUID func_attach_type_guid = {0x85e0d8ef, 0x579e, 0x4931, {0xb0, 0x72, 0x8e, 0xe2, 0x26, 0xbb, 0x2e, 0x9d}};
-#pragma code_seg(push, ".text")
+static GUID BindMonitor_program_type_guid = {
+    0x608c517c, 0x6c52, 0x4a26, {0xb6, 0x77, 0xbb, 0x1c, 0x34, 0x42, 0x5a, 0xdf}};
+static GUID BindMonitor_attach_type_guid = {
+    0xb9707e04, 0x8127, 0x4c72, {0x83, 0x3e, 0x05, 0xb1, 0xfb, 0x43, 0x94, 0x96}};
+#pragma code_seg(push, "bind")
 static uint64_t
-func(void* context)
-#line 17 "sample/custom_program_type/bpf.c"
+BindMonitor(void* context)
+#line 52 "sample/unsafe/invalid_maps3.c"
 {
-#line 17 "sample/custom_program_type/bpf.c"
+#line 52 "sample/unsafe/invalid_maps3.c"
     // Prologue
-#line 17 "sample/custom_program_type/bpf.c"
+#line 52 "sample/unsafe/invalid_maps3.c"
     uint64_t stack[(UBPF_STACK_SIZE + 7) / 8];
-#line 17 "sample/custom_program_type/bpf.c"
+#line 52 "sample/unsafe/invalid_maps3.c"
     register uint64_t r0 = 0;
-#line 17 "sample/custom_program_type/bpf.c"
+#line 52 "sample/unsafe/invalid_maps3.c"
     register uint64_t r1 = 0;
-#line 17 "sample/custom_program_type/bpf.c"
+#line 52 "sample/unsafe/invalid_maps3.c"
     register uint64_t r10 = 0;
 
-#line 17 "sample/custom_program_type/bpf.c"
+#line 52 "sample/unsafe/invalid_maps3.c"
     r1 = (uintptr_t)context;
-#line 17 "sample/custom_program_type/bpf.c"
+#line 52 "sample/unsafe/invalid_maps3.c"
     r10 = (uintptr_t)((uint8_t*)stack + sizeof(stack));
 
-    // EBPF_OP_MOV64_IMM pc=0 dst=r0 src=r0 offset=0 imm=42
-#line 17 "sample/custom_program_type/bpf.c"
-    r0 = IMMEDIATE(42);
+    // EBPF_OP_MOV64_IMM pc=0 dst=r0 src=r0 offset=0 imm=0
+#line 52 "sample/unsafe/invalid_maps3.c"
+    r0 = IMMEDIATE(0);
     // EBPF_OP_EXIT pc=1 dst=r0 src=r0 offset=0 imm=0
-#line 17 "sample/custom_program_type/bpf.c"
+#line 52 "sample/unsafe/invalid_maps3.c"
     return r0;
-#line 17 "sample/custom_program_type/bpf.c"
+#line 52 "sample/unsafe/invalid_maps3.c"
 }
 #pragma code_seg(pop)
 #line __LINE__ __FILE__
@@ -218,17 +237,17 @@ func(void* context)
 static program_entry_t _programs[] = {
     {
         0,
-        func,
-        ".text",
-        ".text",
-        "func",
+        BindMonitor,
+        "bind",
+        "bind",
+        "BindMonitor",
         NULL,
         0,
         NULL,
         0,
         2,
-        &func_program_type_guid,
-        &func_attach_type_guid,
+        &BindMonitor_program_type_guid,
+        &BindMonitor_attach_type_guid,
     },
 };
 #pragma data_seg(pop)
@@ -248,4 +267,5 @@ _get_version(_Out_ bpf2c_version_t* version)
     version->revision = 0;
 }
 
-metadata_table_t bpf_metadata_table = {sizeof(metadata_table_t), _get_programs, _get_maps, _get_hash, _get_version};
+metadata_table_t invalid_maps3_metadata_table = {
+    sizeof(metadata_table_t), _get_programs, _get_maps, _get_hash, _get_version};
