ef1a8036ee166561bd8fd53822e665a0e11857a4
Delaram Amiri <108862804+delaramamiri@users.noreply.github.com>
2023-02-15 16:09:31 -0800

    Update tests/unit/wer_report_test_wrapper.cpp
    Co-authored-by: Dave Thaler <dthaler@microsoft.com>

--- tests/unit/wer_report_test_wrapper.cpp
+++ tests/unit/wer_report_test_wrapper.cpp
@@ -41,7 +41,7 @@ AddVectoredExceptionHandler_test(_In_ unsigned long first, _In_ PVECTORED_EXCEPT
 
 unsigned long SetThreadStackGuarantee_test_stack_size_in_bytes = 0;
 
-// Using BOOL to pass "SetThreadStackGuarantee" 
+// Use BOOL to pass "SetThreadStackGuarantee" 
 // defined in windows kit " processthreadapi.h " file
 BOOL
 SetThreadStackGuarantee_test(_Inout_ unsigned long* stack_size_in_bytes)
