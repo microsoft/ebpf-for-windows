c62edb67d1816f4462e4e2ebd272adf0bab9921c
Gianni Trevisiol <giovanni.trevisiol@gmail.com>
2023-02-22 12:10:18 -0800

    nit

--- include/kernel/ebpf_registry_helper.h
+++ include/kernel/ebpf_registry_helper.h
@@ -34,8 +34,8 @@ close_registry_key(ebpf_registry_key_t key)
     ZwClose(key);
 }
 
-_Success_(return == STATUS_SUCCESS) NTSTATUS static convert_guid_to_string(
-    _In_ const GUID* guid, _Out_writes_all_(string_size) wchar_t* string, size_t string_size)
+_Success_(return == STATUS_SUCCESS) NTSTATUS
+    convert_guid_to_string(_In_ const GUID* guid, _Out_writes_all_(string_size) wchar_t* string, size_t string_size)
 {
     UNICODE_STRING unicode_string = {0};
 
