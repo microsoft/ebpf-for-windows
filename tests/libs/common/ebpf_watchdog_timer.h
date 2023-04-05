225892b2af484ebed1b1342c8c6b2e926b501d89
Alan Jowett <alanjo@microsoft.com>
2023-04-05 12:58:47 -0700

    Manually generated dump on test timeout
    Signed-off-by: Alan Jowett <alanjo@microsoft.com>

--- tests/libs/common/ebpf_watchdog_timer.h
+++ tests/libs/common/ebpf_watchdog_timer.h
@@ -4,7 +4,13 @@
 
 // Winsock2 must be included before windows.h.
 #include <winsock2.h>
-#include <windows.h>
+#include <Windows.h>
+#include <errorrep.h>
+#include <stdexcept>
+#include <stdint.h>
+#include <werapi.h>
+
+#pragma comment(lib, "wer.lib")
 
 #define FILETIME_TICKS_PER_SECOND 10000000LL        // 100 nanoseconds per tick
 #define EBPF_WATCHDOG_TIMER_DUE_TIME_IN_SECONDS 900 // 15 minutes
@@ -19,7 +25,11 @@ typedef class _ebpf_watchdog_timer
     {
         timer = CreateThreadpoolTimer(
             [](_Inout_ PTP_CALLBACK_INSTANCE, _Inout_opt_ PVOID, _Inout_ PTP_TIMER) {
-                RaiseException(STATUS_ASSERTION_FAILURE, 0, 0, NULL);
+                // Attempt to generate a WER report and raise an assertion failure if that fails.
+                if (!generate_wer_report()) {
+                    // This will cause the vectored exception handler to be called.
+                    RaiseException(STATUS_ASSERTION_FAILURE, 0, 0, NULL);
+                }
             },
             NULL,
             NULL);
@@ -41,5 +51,26 @@ typedef class _ebpf_watchdog_timer
     }
 
   private:
+    static bool
+    generate_wer_report()
+    {
+        HREPORT report_handle;
+        HRESULT hr;
+        hr = WerReportCreate(wer_event_type, WerReportApplicationHang, nullptr, &report_handle);
+        if (FAILED(hr)) {
+            return false;
+        }
+        hr = WerReportAddDump(
+            report_handle, GetCurrentProcess(), GetCurrentThread(), WerDumpTypeHeapDump, nullptr, nullptr, 0);
+        if (FAILED(hr)) {
+            return false;
+        }
+        hr = WerReportSubmit(report_handle, WerConsentApproved, 0, nullptr);
+        if (FAILED(hr)) {
+            return false;
+        }
+        return true;
+    }
+    static constexpr const wchar_t wer_event_type[] = L"Test Application Hang";
     PTP_TIMER timer;
 } ebpf_watchdog_timer_t;
