9be5a5cd08ee6cdcf98500a942b718b2e386c578
Alan Jowett <alanjo@microsoft.com>
2023-04-19 09:37:43 -0700

    Extract version from ebpf_version.h
    Signed-off-by: Alan Jowett <alanjo@microsoft.com>

--- scripts/Get-Version.ps1
+++ scripts/Get-Version.ps1
@@ -6,7 +6,19 @@
 
 $content = Get-Content -Path "$PSScriptRoot\..\resource\ebpf_version.h" -Raw -Encoding UTF8
 
-$content = $content.Substring($content.IndexOf("#define EBPF_VERSION") + "#define EBPF_VERSION".Length)
-$content = $content.Substring($content.IndexOf("""") + 1)
-$content = $content.Substring(0, $content.IndexOf(""""))
-$content
+# Extract EBPF_VERSION_MAJOR, EBPF_VERSION_MINOR, and EBPF_VERSION_REVISION from ebpf_version.h.
+
+$major_version_prefix = "#define EBPF_VERSION_MAJOR "
+$minor_version_prefix = "#define EBPF_VERSION_MINOR "
+$revision_version_prefix = "#define EBPF_VERSION_REVISION "
+
+$content -match "$major_version_prefix(\d+)" | Out-Null
+$major_version = $matches[1]
+$content -match "$minor_version_prefix(\d+)" | Out-Null
+$minor_version = $matches[1]
+$content -match "$revision_version_prefix(\d+)" | Out-Null
+$revision_version = $matches[1]
+
+$version = "$major_version.$minor_version.$revision_version"
+
+Write-Output $version
