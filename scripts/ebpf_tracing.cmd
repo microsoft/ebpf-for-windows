a51fc189a1eb64792563e3d64250222a28682729
Gianni Trevisiol <gtrevi@users.noreply.github.com>
2023-04-27 17:36:26 -0700

    revised

--- scripts/ebpf_tracing.cmd
+++ scripts/ebpf_tracing.cmd
@@ -29,9 +29,11 @@ if not exist "!tracePath!" (
 set trace_name="ebpf_diag"
 set logman_period=0:35:00
 set /a max_size_mb=20
-set /a max_size_bytes=max_size_mb*1000000
-set /a files_max_age_days=1
+set /a max_committed_files=41
+
+@rem Internal constants
 set /a num_etl_files_to_keep=1
+set /a max_size_bytes=max_size_mb*1000000
 
 if "%option%"=="periodic" (
 
@@ -46,28 +48,29 @@ if "%option%"=="periodic" (
     netsh wfp show state
     popd
     set "wfp_state_file=!tracePath!\wfpstate.xml"
-	if exist "!wfp_state_file!" (
+    set "wfp_state_file_zip=!tracePath!\wfpstate.zip"
+	makecab "!wfp_state_file!" "!wfp_state_file_zip!"
+	if exist "!wfp_state_file_zip!" (
 		@rem If the file size is less or equal than 'max_size_mb', then move it to the 'traceCommittedPath' directory (otherwise it'll just be overwritten by the next run down).
 		for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
 		set "YYYY=!dt:~0,4!" & set "MM=!dt:~4,2!" & set "DD=!dt:~6,2!"
 		set "HH=!dt:~8,2!" & set "Min=!dt:~10,2!" & set "Sec=!dt:~12,2!"
 		set "timestamp=!YYYY!!MM!!DD!_!HH!!Min!!Sec!"
-		for %%F in ("!wfp_state_file!") do (
+		for %%F in ("!wfp_state_file_zip!") do (
 			if %%~zF LEQ %max_size_bytes% (
-				move /y "!wfp_state_file!" "!traceCommittedPath!\wfpstate_!timestamp!.xml" >nul
+				move /y "!wfp_state_file_zip!" "!traceCommittedPath!\wfpstate_!timestamp!.zip" >nul
 			)
 		)
 	)
 
-	@rem Iterate over all the .etl files in the 'tracePath' directory, sorted in descending order by "date modified",
+	@rem Iterate over all the .etl files in the 'tracePath' directory, sorted in descending order by name,
 	@rem and skip the first 'num_etl_files_to_keep' files (i.e., the newest 'num_etl_files_to_keep' files).
 	for /f "skip=%num_etl_files_to_keep% delims=" %%f in ('dir /b /o-n "!tracePath!\*.etl"') do (
 		move /y "!tracePath!\%%f" "!traceCommittedPath!" >nul
 	)
 
-	@rem Iterate over all the .etl/.xml files in the 'traceCommittedPath' directory, and delete files older than 'files_max_age_days' days.
-	forfiles /p "!traceCommittedPath!" /s /m *.etl /d -%files_max_age_days% /c "cmd /c del @path" >nul
-	forfiles /p "!traceCommittedPath!" /s /m *.xml /d -%files_max_age_days% /c "cmd /c del @path" >nul
+	@rem Iterate over all the files in the 'traceCommittedPath' directory, and delete files older files than `max_committed_files`.
+	for /f "skip=%max_committed_files% delims=" %%f in ('dir /b /o-d "!traceCommittedPath!\*.*"') do ( del "!traceCommittedPath!\%%f" )
 
 ) else if "%option%"=="start" (
 
