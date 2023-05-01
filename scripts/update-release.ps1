cd888c5719bca733917a5704788aaaa56c1f6d23
Gianni Trevisiol <gtrevi@users.noreply.github.com>
2023-05-01 15:53:24 -0700

    Update script

--- scripts/update-release.ps1
+++ scripts/update-release.ps1
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft Corporation
 # SPDX-License-Identifier: MIT
 
-param ($version)
+param ($majorVersion, $minorVersion, $revisionNumber)
 
 # Check if the version number is in the format X.Y.Z
 if ($version -match '^\d+\.\d+\.\d+$') {
@@ -10,14 +10,16 @@ if ($version -match '^\d+\.\d+\.\d+$') {
         # Set the new version number in the ebpf_version.h file.
         $ebpf_version_file = "$PSScriptRoot\..\resource\ebpf_version.h"
         Write-Host -ForegroundColor DarkGreen "Updating the version number in the '$ebpf_version_file' file..."
-        (Get-Content $ebpf_version_file -Raw -Encoding UTF8) -replace 'EBPF_VERSION "\d+\.\d+\.\d+"', "EBPF_VERSION `"$version`"" | Set-Content $ebpf_version_file
-        Write-Host -ForegroundColor DarkGreen "Version number updated to '$version' in $ebpf_version_file"
+        (Get-Content $ebpf_version_file -Raw -Encoding UTF8) -replace 'EBPF_VERSION_MAJOR "\d+"', "EBPF_VERSION_MAJOR `"$majorVersion`"" | Set-Content $ebpf_version_file
+		(Get-Content $ebpf_version_file -Raw -Encoding UTF8) -replace 'EBPF_VERSION_MINOR "\d+"', "EBPF_VERSION_MINOR `"$minorVersion`"" | Set-Content $ebpf_version_file
+		(Get-Content $ebpf_version_file -Raw -Encoding UTF8) -replace 'EBPF_VERSION_REVISION "\d+"', "EBPF_VERSION_REVISION `"$revisionNumber`"" | Set-Content $ebpf_version_file
+        Write-Host -ForegroundColor DarkGreen "Version number updated to '$majorVersion.$minorVersion.$revisionNumber' in $ebpf_version_file"
 
         # Update the version number in the Wix installer file.
         $ebpf_installer_file = "$PSScriptRoot\..\installer\Product.wxs"
         Write-Host -ForegroundColor DarkGreen "Updating the version number in the '$ebpf_installer_file' file..."
-        (Get-Content $ebpf_installer_file -Raw -Encoding UTF8) -replace 'Version="\d+\.\d+\.\d+"', "Version=`"$version`"" | Set-Content $ebpf_installer_file
-        Write-Host -ForegroundColor DarkGreen "Version number updated to '$version' in $ebpf_installer_file"
+        (Get-Content $ebpf_installer_file -Raw -Encoding UTF8) -replace 'Version="\d+\.\d+\.\d+"', "Version=`"$majorVersion.$minorVersion.$revisionNumber`"" | Set-Content $ebpf_installer_file
+        Write-Host -ForegroundColor DarkGreen "Version number updated to '$majorVersion.$minorVersion.$revisionNumber' in $ebpf_installer_file"
 
         # Rebuild the solution, so to regenerate the '.o' files with the new version number.
         Write-Host -ForegroundColor DarkGreen "Rebuilding the solution, please wait..."
@@ -33,8 +35,7 @@ if ($version -match '^\d+\.\d+\.\d+$') {
         .\scripts\generate_expected_bpf2c_output.ps1 .\x64\Debug\
         Write-Host -ForegroundColor DarkGreen "Expected `bpf2c` output regenerated."
 
-        $majorMinor = $version -replace '\.\d+$'
-        Write-Host -ForegroundColor DarkYellow "Please verify all the changes then submit the pull-request into the 'release/$majorMinor' branch."
+        Write-Host -ForegroundColor DarkYellow "Please verify all the changes then submit the pull-request into the 'release/$majorVersion.$minorVersion branch."
     } else {
         Write-Host -ForegroundColor Red "'ebpf-for-windows.sln' not found in the current path."
         Write-Host -ForegroundColor DarkYellow "Please run this script from the root directory of the repository, within a Developer Poweshell for VS 2022."
