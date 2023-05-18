3fd330bef1c7eaea45a5c4c85f2d480e678f2490
Sharmila Palani <shpalan@microsoft.com>
2023-05-18 00:33:25 -0700

    Changed nuget.vcxproj

---
+++ scripts/Set-ManifestVersion.ps1
@@ -0,0 +1,10 @@
+﻿# Copyright (c) Microsoft Corporation
+# SPDX-License-Identifier: MIT
+
+param ($InputFile, $OutputFile)
+
+$version = &"$PSScriptRoot\Get-Version.ps1"
+$new_version = -join($version, ".0")
+$content = Get-Content $InputFile
+$content = $content.Replace("{version}", $new_version)
+set-content $OutputFile $content
