dc4e5dc161b13b7bb529bb1a0b606d0433406d41
Dave Thaler <dthaler@microsoft.com>
2023-06-20 11:06:59 -0700

    Bug fixes
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

--- cmake/settings.cmake
+++ cmake/settings.cmake
@@ -28,7 +28,7 @@ target_compile_definitions("ebpf_for_windows_base_definitions" INTERFACE
   $<$<CONFIG:Debug>:_DEBUG>
   $<$<CONFIG:FuzzerDebug>:_DEBUG>
   $<$<CONFIG:NativeOnlyDebug>:_DEBUG>
-  $<$<CONFIG:NativeOnlyRelease>:_DEBUG>
+  $<$<CONFIG:NativeOnlyRelease>:NDEBUG>
   $<$<CONFIG:Release>:NDEBUG>
   $<$<CONFIG:RelWithDebInfo>:NDEBUG>
 )
