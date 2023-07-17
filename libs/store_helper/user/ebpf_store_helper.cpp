9f7b46128c7e7c93ef6b672b0cc48b70189321d1
Gianni Trevisiol <gtrevi@users.noreply.github.com>
2023-07-17 11:57:47 -0700

    fix

--- libs/store_helper/user/ebpf_store_helper.cpp
+++ libs/store_helper/user/ebpf_store_helper.cpp
@@ -1,10 +1,12 @@
 // Copyright (c) Microsoft Corporation
 // SPDX-License-Identifier: MIT
 
+#ifdef USER_MODE
+#include <windows.h>
+#endif
+
 #include "ebpf_store_helper.h"
 #include "user\ebpf_registry_helper_um.h"
-
-#include <windows.h>
 
 // Incluce the same C code as CPP
 #include "ebpf_store_helper.c"
