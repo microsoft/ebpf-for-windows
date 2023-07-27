6de71837d3be82ecb140db98e7a43273ca1dabdd
Gianni Trevisiol <gtrevi@users.noreply.github.com>
2023-07-26 17:49:13 -0700

    remove cpp wrapper, misc cleanup.

--- include/ebpf_store_helper.h
+++ include/ebpf_store_helper.h
@@ -14,67 +14,76 @@
 
 #define GUID_STRING_LENGTH 38 // not including the null terminator.
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 #ifndef USER_MODE
-typedef HANDLE ebpf_registry_key_t;
+    typedef HANDLE ebpf_registry_key_t;
 #else
 typedef HKEY ebpf_registry_key_t;
 extern ebpf_registry_key_t ebpf_root_registry_key;
 #endif
 
-/**
- * @brief Open or create the root of the eBPF registry store.
- *
- * @param[in] provider_key Pointer to the registry key to be initialized.
- *
- * @returns  Status of the operation.
- */
-ebpf_result_t
-ebpf_store_open_or_create_provider_registry_key(_Out_ ebpf_registry_key_t* provider_key);
+    /**
+     * @brief Open or create the root of the eBPF registry store.
+     *
+     * @param[in] provider_key Pointer to the registry key to be initialized.
+     *
+     * @returns  Status of the operation.
+     */
+    ebpf_result_t
+    ebpf_store_open_or_create_provider_registry_key(_Out_ ebpf_registry_key_t* provider_key);
 
-/**
- * @brief Update the provider prototype information in the eBPF registry store.
- *
- * @param[in] helper_info_key Pointer to the registry key to be initialized.
- * @param[in] helper_info Pointer to the helper function prototype.
- *
- * @return Status of the operation.
- */
-ebpf_result_t
-ebpf_store_update_helper_prototype(
-    ebpf_registry_key_t helper_info_key, _In_ const ebpf_helper_function_prototype_t* helper_info);
+    /**
+     * @brief Update the provider prototype information in the eBPF registry store.
+     *
+     * @param[in] helper_info_key Pointer to the registry key to be initialized.
+     * @param[in] helper_info Pointer to the helper function prototype.
+     *
+     * @return Status of the operation.
+     */
+    ebpf_result_t
+    ebpf_store_update_helper_prototype(
+        ebpf_registry_key_t helper_info_key, _In_ const ebpf_helper_function_prototype_t* helper_info);
 
-/**
- * @brief Update section information in eBPF store.
- *
- * @param[in] section_info Pointer to an array of section information.
- * @param[in] section_info_count Count of section information entries.
- *
- * @returns Status of the operation.
- */
-ebpf_result_t
-ebpf_store_update_section_information(
-    _In_reads_(section_info_count) const ebpf_program_section_info_t* section_info, uint32_t section_info_count);
+    /**
+     * @brief Update section information in eBPF store.
+     *
+     * @param[in] section_info Pointer to an array of section information.
+     * @param[in] section_info_count Count of section information entries.
+     *
+     * @returns Status of the operation.
+     */
+    ebpf_result_t
+    ebpf_store_update_section_information(
+        _In_reads_(section_info_count) const ebpf_program_section_info_t* section_info, uint32_t section_info_count);
 
-/**
- * @brief Update program information in eBPF store.
- *
- * @param[in] program_info Pointer to an array of program information.
- * @param[in] program_info_count Count of program information entries.
- *
- * @returns Status of the operation.
- */
-ebpf_result_t
-ebpf_store_update_program_information(
-    _In_reads_(program_info_count) const ebpf_program_info_t* program_info, uint32_t program_info_count);
+    /**
+     * @brief Update program information in eBPF store.
+     *
+     * @param[in] program_info Pointer to an array of program information.
+     * @param[in] program_info_count Count of program information entries.
+     *
+     * @returns Status of the operation.
+     */
+    ebpf_result_t
+    ebpf_store_update_program_information(
+        _In_reads_(program_info_count) const ebpf_program_info_t* program_info, uint32_t program_info_count);
 
-/**
- * @brief Update global helper information in eBPF store.
- *
- * @param[in] helper_info Pointer to an array of helper function prototypes.
- * @param[in] helper_info_count Count of helper function prototypes.
- *
- * @returns Status of the operation.
- */
-ebpf_result_t
-ebpf_store_update_global_helper_information(
-    _In_reads_(helper_info_count) ebpf_helper_function_prototype_t* helper_info, uint32_t helper_info_count);
+    /**
+     * @brief Update global helper information in eBPF store.
+     *
+     * @param[in] helper_info Pointer to an array of helper function prototypes.
+     * @param[in] helper_info_count Count of helper function prototypes.
+     *
+     * @returns Status of the operation.
+     */
+    ebpf_result_t
+    ebpf_store_update_global_helper_information(
+        _In_reads_(helper_info_count) ebpf_helper_function_prototype_t* helper_info, uint32_t helper_info_count);
+
+#ifdef __cplusplus
+}
+#endif
--- libs/store_helper/user/ebpf_store_helper.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Microsoft Corporation
-// SPDX-License-Identifier: MIT
-
-// Include the same C code as CPP
-#include "..\ebpf_store_helper.c"
