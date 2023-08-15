c8d02386e0706b1066af369c3a1fe1450d3452a2
Sharmila Palani <shpalan@microsoft.com>
2023-08-15 12:58:42 -0700

    Initial file

---
+++ scripts/Set-Version-File.ps1
@@ -0,0 +1,9 @@
+﻿# Copyright (c) Microsoft Corporation
+# SPDX-License-Identifier: MIT
+
+param ($InputFile)
+
+$version = &"$PSScriptRoot\Get-Version.ps1"
+$file = Get-Item $InputFile
+
+Rename-Item -Path $InputFile -NewName "$file.Basename.$version.$file.Extension"
