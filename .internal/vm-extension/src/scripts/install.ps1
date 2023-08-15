a93db8e444a0120ffd723ff2b0294dd8d83fabf3
Gianni Trevisiol <gtrevi@users.noreply.github.com>
2023-08-14 18:17:09 -0700

    wip

--- .internal/vm-extension/src/scripts/install.ps1
+++ .internal/vm-extension/src/scripts/install.ps1
@@ -1,9 +1,11 @@
 # Dot source the utility script
 . .\utilities.ps1
 
-# Install eBPF from the artifact package within the extension ZIP file
-# Since the VM Agent does not know if eBPF was already provisioned by WinPA, it may call install
+# Install or Update eBPF from the artifact package within the extension ZIP file
+#
+# NOTE: Since the VM Agent does not know if eBPF was already provisioned by WinPA, it may call install
 # eventhough it would be an update. Therefore, we always update we always call InstallOrUpdate-eBPF
 # so we check if the eBPF package is already installed.
+#
 # NOTE: The status file will be generated within the function, given its variable scope.
 InstallOrUpdate-eBPF $OperationNameInstall $EbpfPackagePath, $EbpfDefaultInstallPath
--- .internal/vm-extension/src/scripts/update.ps1
+++ .internal/vm-extension/src/scripts/update.ps1
@@ -5,10 +5,16 @@
 Create-StatusFile -handlerWorkloadName $HandlerWorkloadName -operationName $OperationNameUpdate -status $StatusTransitioning -statusCode 0 -statusMessage "Starting eBPF update"
 
 # NOTE: The status file will be generated within the function, given its variable scope.
-$res = InstallOrUpdate-eBPF $OperationNameUpdate, $EbpfPackagePath, $EbpfDefaultInstallPath
+InstallOrUpdate-eBPF $OperationNameUpdate, $EbpfPackagePath, $EbpfDefaultInstallPath
 
-# Extra step for IMDS (this is not accounted as a result of the eBPF VM Extension): Start ProxyAgent
-$res = Start-Service -Name "GuestProxyAgent"
-if ($res -ne 0) {
-    Write-Log -level $LogLevelError -message "Failed to start GuestProxyAgent. Error code: $res"
+# Extra step for IMDS (this is not accounted as a result of the eBPF VM Extension, as it is not part of the eBPF VM Extension package)
+$serviceName = "GuestProxyAgent"
+$service = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
+if ($null -eq $service) {
+    Write-Log -level $LogLevelInfo -message "$serviceName does not exist -> skipping start."
+} else {
+    $res = Start-Service -Name $serviceName -ErrorAction SilentlyContinue
+    if ($null -ne $res) {
+        Write-Log -level $LogLevelError -message "Failed to start $serviceName. Error code: $res"
+    }
 }
