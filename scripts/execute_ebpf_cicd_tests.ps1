0259aa062cbcda5f0cd69df72d2f69b31c0c17f5
Matt Ige <maige@microsoft.com>
2023-09-14 15:18:58 -0700

    revert test script modifications

--- scripts/execute_ebpf_cicd_tests.ps1
+++ scripts/execute_ebpf_cicd_tests.ps1
@@ -24,27 +24,27 @@ Import-Module .\vm_run_tests.psm1  -Force -ArgumentList ($AdminTestVMCredential.
 $Config = Get-Content ("{0}\{1}" -f $PSScriptRoot, $TestExecutionJsonFileName) | ConvertFrom-Json
 $VMList = $Config.VMMap.$SelfHostedRunnerName
 
-# # Run tests on test VMs.
-# foreach ($VM in $VMList) {
-#     Invoke-CICDTestsOnVM `
-#         -VMName $VM.Name `
-#         -Coverage $Coverage `
-#         -TestMode $TestMode `
-#         -Options $Options
-# }
+# Run tests on test VMs.
+foreach ($VM in $VMList) {
+    Invoke-CICDTestsOnVM `
+        -VMName $VM.Name `
+        -Coverage $Coverage `
+        -TestMode $TestMode `
+        -Options $Options
+}
 
 # This script is used to execute the various kernel mode tests. The required behavior is selected by the $TestMode
 # parameter.
 if ($TestMode -eq "CI/CD") {
 
-    # # Run XDP Tests.
-    # Invoke-XDPTestsOnVM $Config.Interfaces $VMList[0].Name
+    # Run XDP Tests.
+    Invoke-XDPTestsOnVM $Config.Interfaces $VMList[0].Name
 
     # Run Connect Redirect Tests.
     Invoke-ConnectRedirectTestsOnVM $Config.Interfaces $Config.ConnectRedirectTest `
         -UserType "Administrator" $VMList[0].Name
-    # Invoke-ConnectRedirectTestsOnVM $Config.Interfaces $Config.ConnectRedirectTest `
-    #     -UserType "StandardUser" $VMList[0].Name
+    Invoke-ConnectRedirectTestsOnVM $Config.Interfaces $Config.ConnectRedirectTest `
+        -UserType "StandardUser" $VMList[0].Name
 }
 
 # Stop eBPF components on test VMs.
