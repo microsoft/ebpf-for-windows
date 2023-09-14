cff6690c5f45c9d92ec465657d30fe12224f71d9
Matt Ige <maige@microsoft.com>
2023-09-14 11:10:46 -0700

    make tests run faster for now

--- scripts/setup_ebpf_cicd_tests.ps1
+++ scripts/setup_ebpf_cicd_tests.ps1
@@ -30,8 +30,8 @@ Remove-Item ".\TestLogs" -Recurse -Confirm:$false -ErrorAction SilentlyContinue
 # Get all VMs to ready state.
 Initialize-AllVMs -VMList $VMList -ErrorAction Stop
 
-# Download the release artifacts for regression tests.
-Get-RegressionTestArtifacts
+# # Download the release artifacts for regression tests.
+# Get-RegressionTestArtifacts
 
 Get-Duonic
 
