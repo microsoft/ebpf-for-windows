64602499cf361d719a04cb0a94bb117a239d51de
Gianni Trevisiol <gtrevi@users.noreply.github.com>
2023-09-29 17:22:16 -0700

    sync

--- .internal/vm-extension/src/scripts/uninstall.ps1
+++ .internal/vm-extension/src/scripts/uninstall.ps1
@@ -1,5 +1,5 @@
 # Dot source the utility script
-. .\common.ps1
+. .\scripts\common.ps1
 
 # Remove a handler from the VM (Disable and Uninstall):
 # https://github.com/Azure/azure-vmextension-publishing/wiki/2.0-Partner-Guide-Handler-Design-Details#222-remove-a-handler-from-the-vm-disable-and-uninstall
