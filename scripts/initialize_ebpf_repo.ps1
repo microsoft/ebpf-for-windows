b677df2fb49b466e86418605c592de92f2df086d
Will Hawkins <hawkinsw@obs.cr>
2023-12-08 15:18:28 -0500

    WIP: Testing update to build instructions.
    Signed-off-by: Will Hawkins <hawkinsw@obs.cr>

--- scripts/initialize_ebpf_repo.ps1
+++ scripts/initialize_ebpf_repo.ps1
@@ -4,9 +4,10 @@
 # Define the commands to run
 $commands = @(
     "git submodule update --init --recursive",
+    "cmake -G 'Visual Studio 17 2022' -S external\ubpf -B external\ubpf\build",
+    "cmake --build external\ubpf\build --target Conformance-download",
     "cmake -G 'Visual Studio 17 2022' -S external\ebpf-verifier -B external\ebpf-verifier\build",
     "cmake -G 'Visual Studio 17 2022' -S external\catch2 -B external\catch2\build -DBUILD_TESTING=OFF",
-    "cmake -G 'Visual Studio 17 2022' -S external\ubpf -B external\ubpf\build",
     "nuget restore ebpf-for-windows.sln"
 )
 
