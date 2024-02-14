b72a1ec00ae4a8d92d759b1ac2d7b500aa6d39c2
Sharmila Palani (from Dev Box) <shpalan@microsoft.com>
2024-02-14 14:31:24 -0800

    Cherry-pick-PR3260

--- tests/bpf2c_tests/expected/tail_call_sequential_raw.c
+++ tests/bpf2c_tests/expected/tail_call_sequential_raw.c
@@ -6770,7 +6770,7 @@ _get_version(_Out_ bpf2c_version_t* version)
 {
     version->major = 0;
     version->minor = 14;
-    version->revision = 0;
+    version->revision = 1;
 }
 
 #pragma data_seg(push, "map_initial_values")
