6be804aab2b02351efe76f831582ad95bd3a68bc
Shankar Seal <sseal@microsoft.com>
2024-04-02 19:00:06 -0700

    p# This is a combination of 2 commits.
    update release version to 0.16.0

--- resource/ebpf_version.h
+++ resource/ebpf_version.h
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 
 #define EBPF_VERSION_MAJOR 0
-#define EBPF_VERSION_MINOR 15
+#define EBPF_VERSION_MINOR 16
 #define EBPF_VERSION_REVISION 0
 
 #define QUOTE(str) #str
