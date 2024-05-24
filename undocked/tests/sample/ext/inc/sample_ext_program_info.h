f7ec1683406e9465f8cf04c43ea4fb8844a95de6
Anurag Saxena <43585259+saxena-anurag@users.noreply.github.com>
2024-05-23 18:05:36 -0700

    Add `total_size` to extension header. (#3573)
    * change ebpf_extension_header_t to ebpf_version_header_t
    
    * revert to ebpf_version_header_t
    
    * revert to ebpf_version_header_t
    
    * update header
    
    * update doc
    
    * update doc
    
    * use total_size to get the actual struct size
    
    * Apply suggestions from code review
    
    Co-authored-by: Dave Thaler <dthaler1968@gmail.com>
    
    ---------
    
    Co-authored-by: Dave Thaler <dthaler1968@gmail.com>

--- undocked/tests/sample/ext/inc/sample_ext_program_info.h
+++ undocked/tests/sample/ext/inc/sample_ext_program_info.h
@@ -23,12 +23,12 @@ static const ebpf_context_descriptor_t _sample_ebpf_context_descriptor = {
 
 // Sample Extension Helper function prototype descriptors.
 static const ebpf_helper_function_prototype_t _sample_ebpf_extension_helper_function_prototype[] = {
-    {{EBPF_HELPER_FUNCTION_PROTOTYPE_CURRENT_VERSION, EBPF_HELPER_FUNCTION_PROTOTYPE_CURRENT_VERSION_SIZE},
+    {EBPF_HELPER_FUNCTION_PROTOTYPE_HEADER,
      SAMPLE_EXT_HELPER_FUNCTION_START + 1,
      "sample_ebpf_extension_helper_function1",
      EBPF_RETURN_TYPE_INTEGER,
      {EBPF_ARGUMENT_TYPE_PTR_TO_CTX}},
-    {{EBPF_HELPER_FUNCTION_PROTOTYPE_CURRENT_VERSION, EBPF_HELPER_FUNCTION_PROTOTYPE_CURRENT_VERSION_SIZE},
+    {EBPF_HELPER_FUNCTION_PROTOTYPE_HEADER,
      SAMPLE_EXT_HELPER_FUNCTION_START + 2,
      "sample_ebpf_extension_find",
      EBPF_RETURN_TYPE_INTEGER,
@@ -36,7 +36,7 @@ static const ebpf_helper_function_prototype_t _sample_ebpf_extension_helper_func
       EBPF_ARGUMENT_TYPE_CONST_SIZE,
       EBPF_ARGUMENT_TYPE_PTR_TO_READABLE_MEM,
       EBPF_ARGUMENT_TYPE_CONST_SIZE}},
-    {{EBPF_HELPER_FUNCTION_PROTOTYPE_CURRENT_VERSION, EBPF_HELPER_FUNCTION_PROTOTYPE_CURRENT_VERSION_SIZE},
+    {EBPF_HELPER_FUNCTION_PROTOTYPE_HEADER,
      SAMPLE_EXT_HELPER_FUNCTION_START + 3,
      "sample_ebpf_extension_replace",
      EBPF_RETURN_TYPE_INTEGER,
@@ -49,7 +49,7 @@ static const ebpf_helper_function_prototype_t _sample_ebpf_extension_helper_func
 // Global helper function prototype descriptors.
 static const ebpf_helper_function_prototype_t _sample_ebpf_extension_global_helper_function_prototype[] = {
     {
-        {EBPF_HELPER_FUNCTION_PROTOTYPE_CURRENT_VERSION, EBPF_HELPER_FUNCTION_PROTOTYPE_CURRENT_VERSION_SIZE},
+        EBPF_HELPER_FUNCTION_PROTOTYPE_HEADER,
         BPF_FUNC_get_current_pid_tgid,
         "bpf_get_current_pid_tgid",
         EBPF_RETURN_TYPE_INTEGER,
@@ -57,13 +57,13 @@ static const ebpf_helper_function_prototype_t _sample_ebpf_extension_global_help
 };
 
 static const ebpf_program_type_descriptor_t _sample_ebpf_extension_program_type_descriptor = {
-    {EBPF_PROGRAM_TYPE_DESCRIPTOR_CURRENT_VERSION, EBPF_PROGRAM_TYPE_DESCRIPTOR_CURRENT_VERSION_SIZE},
+    EBPF_PROGRAM_TYPE_DESCRIPTOR_HEADER,
     "sample",
     &_sample_ebpf_context_descriptor,
     EBPF_PROGRAM_TYPE_SAMPLE_GUID,
     BPF_PROG_TYPE_SAMPLE};
 static const ebpf_program_info_t _sample_ebpf_extension_program_info = {
-    {EBPF_PROGRAM_INFORMATION_CURRENT_VERSION, EBPF_PROGRAM_INFORMATION_CURRENT_VERSION_SIZE},
+    EBPF_PROGRAM_INFORMATION_HEADER,
     &_sample_ebpf_extension_program_type_descriptor,
     EBPF_COUNT_OF(_sample_ebpf_extension_helper_function_prototype),
     _sample_ebpf_extension_helper_function_prototype,
