b9d6cb6b7edcc5314413d866a63d36ebc41ab14d
Shankar Seal <74580197+shankarseal@users.noreply.github.com>
2024-07-15 11:43:20 -0700

    update version to 0.18.0 (#3690)

--- tests/bpf2c_tests/expected/xdp_datasize_unsafe_sys.c
+++ tests/bpf2c_tests/expected/xdp_datasize_unsafe_sys.c
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 
 // Do not alter this generated file.
-// This file was generated from tail_call.o
+// This file was generated from xdp_datasize_unsafe.o
 
 #define NO_CRT
 #include "bpf2c.h"
@@ -15,7 +15,7 @@ DRIVER_INITIALIZE DriverEntry;
 DRIVER_UNLOAD DriverUnload;
 RTL_QUERY_REGISTRY_ROUTINE static _bpf2c_query_registry_routine;
 
-#define metadata_table tail_call##_metadata_table
+#define metadata_table xdp_datasize_unsafe##_metadata_table
 
 static GUID _bpf2c_npi_id = {/* c847aac8-a6f2-4b53-aea3-f4a94b9a80cb */
                              0xc847aac8,
@@ -173,177 +173,89 @@ _get_hash(_Outptr_result_buffer_maybenull_(*size) const uint8_t** hash, _Out_ si
     *hash = NULL;
     *size = 0;
 }
-#pragma data_seg(push, "maps")
-static map_entry_t _maps[] = {
-    {NULL,
-     {
-         BPF_MAP_TYPE_PROG_ARRAY, // Type of map.
-         4,                       // Size in bytes of a map key.
-         4,                       // Size in bytes of a map value.
-         10,                      // Maximum number of entries allowed in the map.
-         0,                       // Inner map index.
-         LIBBPF_PIN_NONE,         // Pinning type for the map.
-         10,                      // Identifier for a map template.
-         0,                       // The id of the inner map template.
-     },
-     "map"},
-    {NULL,
-     {
-         BPF_MAP_TYPE_ARRAY, // Type of map.
-         4,                  // Size in bytes of a map key.
-         4,                  // Size in bytes of a map value.
-         1,                  // Maximum number of entries allowed in the map.
-         0,                  // Inner map index.
-         LIBBPF_PIN_NONE,    // Pinning type for the map.
-         16,                 // Identifier for a map template.
-         0,                  // The id of the inner map template.
-     },
-     "canary"},
-};
-#pragma data_seg(pop)
-
 static void
 _get_maps(_Outptr_result_buffer_maybenull_(*count) map_entry_t** maps, _Out_ size_t* count)
 {
-    *maps = _maps;
-    *count = 2;
+    *maps = NULL;
+    *count = 0;
 }
 
-static GUID callee_program_type_guid = {0xf788ef4a, 0x207d, 0x4dc3, {0x85, 0xcf, 0x0f, 0x2e, 0xa1, 0x07, 0x21, 0x3c}};
-static GUID callee_attach_type_guid = {0xf788ef4b, 0x207d, 0x4dc3, {0x85, 0xcf, 0x0f, 0x2e, 0xa1, 0x07, 0x21, 0x3c}};
-#pragma code_seg(push, "sample~1")
+static GUID unsafe_program_program_type_guid = {
+    0xf1832a85, 0x85d5, 0x45b0, {0x98, 0xa0, 0x70, 0x69, 0xd6, 0x30, 0x13, 0xb0}};
+static GUID unsafe_program_attach_type_guid = {
+    0x85e0d8ef, 0x579e, 0x4931, {0xb0, 0x72, 0x8e, 0xe2, 0x26, 0xbb, 0x2e, 0x9d}};
+#pragma code_seg(push, "xdp")
 static uint64_t
-callee(void* context)
-#line 49 "sample/undocked/tail_call.c"
+unsafe_program(void* context)
+#line 26 "sample/unsafe/xdp_datasize_unsafe.c"
 {
-#line 49 "sample/undocked/tail_call.c"
+#line 26 "sample/unsafe/xdp_datasize_unsafe.c"
     // Prologue
-#line 49 "sample/undocked/tail_call.c"
+#line 26 "sample/unsafe/xdp_datasize_unsafe.c"
     uint64_t stack[(UBPF_STACK_SIZE + 7) / 8];
-#line 49 "sample/undocked/tail_call.c"
+#line 26 "sample/unsafe/xdp_datasize_unsafe.c"
     register uint64_t r0 = 0;
-#line 49 "sample/undocked/tail_call.c"
+#line 26 "sample/unsafe/xdp_datasize_unsafe.c"
     register uint64_t r1 = 0;
-#line 49 "sample/undocked/tail_call.c"
-    register uint64_t r10 = 0;
-
-#line 49 "sample/undocked/tail_call.c"
-    r1 = (uintptr_t)context;
-#line 49 "sample/undocked/tail_call.c"
-    r10 = (uintptr_t)((uint8_t*)stack + sizeof(stack));
-
-    // EBPF_OP_MOV64_IMM pc=0 dst=r0 src=r0 offset=0 imm=42
-#line 49 "sample/undocked/tail_call.c"
-    r0 = IMMEDIATE(42);
-    // EBPF_OP_EXIT pc=1 dst=r0 src=r0 offset=0 imm=0
-#line 49 "sample/undocked/tail_call.c"
-    return r0;
-#line 49 "sample/undocked/tail_call.c"
-}
-#pragma code_seg(pop)
-#line __LINE__ __FILE__
-
-static helper_function_entry_t caller_helpers[] = {
-    {NULL, 5, "helper_id_5"},
-    {NULL, 1, "helper_id_1"},
-};
-
-static GUID caller_program_type_guid = {0xf788ef4a, 0x207d, 0x4dc3, {0x85, 0xcf, 0x0f, 0x2e, 0xa1, 0x07, 0x21, 0x3c}};
-static GUID caller_attach_type_guid = {0xf788ef4b, 0x207d, 0x4dc3, {0x85, 0xcf, 0x0f, 0x2e, 0xa1, 0x07, 0x21, 0x3c}};
-static uint16_t caller_maps[] = {
-    0,
-    1,
-};
-
-#pragma code_seg(push, "sample~2")
-static uint64_t
-caller(void* context)
-#line 33 "sample/undocked/tail_call.c"
-{
-#line 33 "sample/undocked/tail_call.c"
-    // Prologue
-#line 33 "sample/undocked/tail_call.c"
-    uint64_t stack[(UBPF_STACK_SIZE + 7) / 8];
-#line 33 "sample/undocked/tail_call.c"
-    register uint64_t r0 = 0;
-#line 33 "sample/undocked/tail_call.c"
-    register uint64_t r1 = 0;
-#line 33 "sample/undocked/tail_call.c"
+#line 26 "sample/unsafe/xdp_datasize_unsafe.c"
     register uint64_t r2 = 0;
-#line 33 "sample/undocked/tail_call.c"
+#line 26 "sample/unsafe/xdp_datasize_unsafe.c"
     register uint64_t r3 = 0;
-#line 33 "sample/undocked/tail_call.c"
-    register uint64_t r4 = 0;
-#line 33 "sample/undocked/tail_call.c"
-    register uint64_t r5 = 0;
-#line 33 "sample/undocked/tail_call.c"
+#line 26 "sample/unsafe/xdp_datasize_unsafe.c"
     register uint64_t r10 = 0;
 
-#line 33 "sample/undocked/tail_call.c"
+#line 26 "sample/unsafe/xdp_datasize_unsafe.c"
     r1 = (uintptr_t)context;
-#line 33 "sample/undocked/tail_call.c"
+#line 26 "sample/unsafe/xdp_datasize_unsafe.c"
     r10 = (uintptr_t)((uint8_t*)stack + sizeof(stack));
 
-    // EBPF_OP_MOV64_IMM pc=0 dst=r2 src=r0 offset=0 imm=0
-#line 33 "sample/undocked/tail_call.c"
-    r2 = IMMEDIATE(0);
-    // EBPF_OP_STXW pc=1 dst=r10 src=r2 offset=-4 imm=0
-#line 35 "sample/undocked/tail_call.c"
-    *(uint32_t*)(uintptr_t)(r10 + OFFSET(-4)) = (uint32_t)r2;
-    // EBPF_OP_LDDW pc=2 dst=r2 src=r1 offset=0 imm=1
-#line 38 "sample/undocked/tail_call.c"
-    r2 = POINTER(_maps[0].address);
-    // EBPF_OP_MOV64_IMM pc=4 dst=r3 src=r0 offset=0 imm=9
-#line 38 "sample/undocked/tail_call.c"
-    r3 = IMMEDIATE(9);
-    // EBPF_OP_CALL pc=5 dst=r0 src=r0 offset=0 imm=5
-#line 38 "sample/undocked/tail_call.c"
-    r0 = caller_helpers[0].address(r1, r2, r3, r4, r5);
-#line 38 "sample/undocked/tail_call.c"
-    if ((caller_helpers[0].tail_call) && (r0 == 0)) {
-#line 38 "sample/undocked/tail_call.c"
-        return 0;
-#line 38 "sample/undocked/tail_call.c"
-    }
-    // EBPF_OP_MOV64_REG pc=6 dst=r2 src=r10 offset=0 imm=0
-#line 38 "sample/undocked/tail_call.c"
-    r2 = r10;
-    // EBPF_OP_ADD64_IMM pc=7 dst=r2 src=r0 offset=0 imm=-4
-#line 38 "sample/undocked/tail_call.c"
-    r2 += IMMEDIATE(-4);
-    // EBPF_OP_LDDW pc=8 dst=r1 src=r1 offset=0 imm=2
-#line 41 "sample/undocked/tail_call.c"
-    r1 = POINTER(_maps[1].address);
-    // EBPF_OP_CALL pc=10 dst=r0 src=r0 offset=0 imm=1
-#line 41 "sample/undocked/tail_call.c"
-    r0 = caller_helpers[1].address(r1, r2, r3, r4, r5);
-#line 41 "sample/undocked/tail_call.c"
-    if ((caller_helpers[1].tail_call) && (r0 == 0)) {
-#line 41 "sample/undocked/tail_call.c"
-        return 0;
-#line 41 "sample/undocked/tail_call.c"
-    }
-    // EBPF_OP_JEQ_IMM pc=11 dst=r0 src=r0 offset=2 imm=0
-#line 42 "sample/undocked/tail_call.c"
-    if (r0 == IMMEDIATE(0)) {
-#line 42 "sample/undocked/tail_call.c"
+    // EBPF_OP_MOV64_IMM pc=0 dst=r0 src=r0 offset=0 imm=1
+#line 26 "sample/unsafe/xdp_datasize_unsafe.c"
+    r0 = IMMEDIATE(1);
+    // EBPF_OP_LDXW pc=1 dst=r2 src=r1 offset=0 imm=0
+#line 20 "sample/unsafe/xdp_datasize_unsafe.c"
+    r2 = *(uint32_t*)(uintptr_t)(r1 + OFFSET(0));
+    // EBPF_OP_LDXDW pc=2 dst=r1 src=r1 offset=8 imm=0
+#line 32 "sample/unsafe/xdp_datasize_unsafe.c"
+    r1 = *(uint64_t*)(uintptr_t)(r1 + OFFSET(8));
+    // EBPF_OP_MOV64_REG pc=3 dst=r3 src=r2 offset=0 imm=0
+#line 32 "sample/unsafe/xdp_datasize_unsafe.c"
+    r3 = r2;
+    // EBPF_OP_ADD64_IMM pc=4 dst=r3 src=r0 offset=0 imm=14
+#line 32 "sample/unsafe/xdp_datasize_unsafe.c"
+    r3 += IMMEDIATE(14);
+    // EBPF_OP_JGT_REG pc=5 dst=r3 src=r1 offset=4 imm=0
+#line 32 "sample/unsafe/xdp_datasize_unsafe.c"
+    if (r3 > r1) {
+#line 32 "sample/unsafe/xdp_datasize_unsafe.c"
         goto label_1;
-#line 42 "sample/undocked/tail_call.c"
+#line 32 "sample/unsafe/xdp_datasize_unsafe.c"
     }
-    // EBPF_OP_MOV64_IMM pc=12 dst=r1 src=r0 offset=0 imm=1
-#line 42 "sample/undocked/tail_call.c"
-    r1 = IMMEDIATE(1);
-    // EBPF_OP_STXW pc=13 dst=r0 src=r1 offset=0 imm=0
-#line 43 "sample/undocked/tail_call.c"
-    *(uint32_t*)(uintptr_t)(r0 + OFFSET(0)) = (uint32_t)r1;
+    // EBPF_OP_LDXH pc=6 dst=r1 src=r2 offset=12 imm=0
+#line 38 "sample/unsafe/xdp_datasize_unsafe.c"
+    r1 = *(uint16_t*)(uintptr_t)(r2 + OFFSET(12));
+    // EBPF_OP_JEQ_IMM pc=7 dst=r1 src=r0 offset=2 imm=8
+#line 38 "sample/unsafe/xdp_datasize_unsafe.c"
+    if (r1 == IMMEDIATE(8)) {
+#line 38 "sample/unsafe/xdp_datasize_unsafe.c"
+        goto label_1;
+#line 38 "sample/unsafe/xdp_datasize_unsafe.c"
+    }
+    // EBPF_OP_JEQ_IMM pc=8 dst=r1 src=r0 offset=1 imm=56710
+#line 38 "sample/unsafe/xdp_datasize_unsafe.c"
+    if (r1 == IMMEDIATE(56710)) {
+#line 38 "sample/unsafe/xdp_datasize_unsafe.c"
+        goto label_1;
+#line 38 "sample/unsafe/xdp_datasize_unsafe.c"
+    }
+    // EBPF_OP_MOV64_IMM pc=9 dst=r0 src=r0 offset=0 imm=2
+#line 38 "sample/unsafe/xdp_datasize_unsafe.c"
+    r0 = IMMEDIATE(2);
 label_1:
-    // EBPF_OP_MOV64_IMM pc=14 dst=r0 src=r0 offset=0 imm=6
-#line 46 "sample/undocked/tail_call.c"
-    r0 = IMMEDIATE(6);
-    // EBPF_OP_EXIT pc=15 dst=r0 src=r0 offset=0 imm=0
-#line 46 "sample/undocked/tail_call.c"
+    // EBPF_OP_EXIT pc=10 dst=r0 src=r0 offset=0 imm=0
+#line 43 "sample/unsafe/xdp_datasize_unsafe.c"
     return r0;
-#line 46 "sample/undocked/tail_call.c"
+#line 43 "sample/unsafe/xdp_datasize_unsafe.c"
 }
 #pragma code_seg(pop)
 #line __LINE__ __FILE__
@@ -352,31 +264,17 @@ label_1:
 static program_entry_t _programs[] = {
     {
         0,
-        callee,
-        "sample~1",
-        "sample_ext/0",
-        "callee",
+        unsafe_program,
+        "xdp",
+        "xdp",
+        "unsafe_program",
         NULL,
         0,
         NULL,
         0,
-        2,
-        &callee_program_type_guid,
-        &callee_attach_type_guid,
-    },
-    {
-        0,
-        caller,
-        "sample~2",
-        "sample_ext",
-        "caller",
-        caller_maps,
-        2,
-        caller_helpers,
-        2,
-        16,
-        &caller_program_type_guid,
-        &caller_attach_type_guid,
+        11,
+        &unsafe_program_program_type_guid,
+        &unsafe_program_attach_type_guid,
     },
 };
 #pragma data_seg(pop)
@@ -385,7 +283,7 @@ static void
 _get_programs(_Outptr_result_buffer_(*count) program_entry_t** programs, _Out_ size_t* count)
 {
     *programs = _programs;
-    *count = 2;
+    *count = 1;
 }
 
 static void
@@ -403,5 +301,5 @@ _get_map_initial_values(_Outptr_result_buffer_(*count) map_initial_values_t** ma
     *count = 0;
 }
 
-metadata_table_t tail_call_metadata_table = {
+metadata_table_t xdp_datasize_unsafe_metadata_table = {
     sizeof(metadata_table_t), _get_programs, _get_maps, _get_hash, _get_version, _get_map_initial_values};
