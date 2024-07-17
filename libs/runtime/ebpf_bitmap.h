d7ba119d3ba6172e47f878207120d2cf5ff9bb19
Michael Agun <danielagun@microsoft.com>
2024-07-17 23:21:52 +0000

    Merged PR 30986: Add LPM map unit tests, fix lookup and input checking bugs
    ## Description
    
    This improves and adds ebpf map unit testing and fixes some bugs.
    
    Changes:
    - `_create_lpm_map`: fixes length calculation for key size bitmap
      - `max_prefix` should be the passed value, bitmap size gets `+1` to account for max length keys
    - `_find_lpm_map_entry`: initialize search from search key bit length instead of maximum stored key length
      - reduces number of hashes to try during lookup (for short keys) and fixes bug of looking for map key longer than passed search key
      - adds `ebpf_bitmap_start_reverse_search_at` to initialize set bit search to a specific bit index
    - `_create_array_map_with_map_struct_size`: argument check for value size/capacity combination that allocates >128GB
    - `_next_lpm_map_key_and_value`/`_next_hash_map_key_and_value` - fix SAL annotation for `next_key` (should be `_Inout_`)
    - Refactor lpm key handling to use new `ebpf_core_lpm_key_t` struct instead of pointer to prefix length
    - Add, fix, and extend LPM map unit tests
    
    ## Testing
    
    Extends the `map_crud_operations_lpm_trie_32` test case and adds a second `[negative]` tagged version which tests operations that shouldn't work (including some edge cases that were previously missed).
    
    `map_create_invalid` test case added to try creating various maps with invalid parameters.
    
    Extends and refactors `map_crud_operations_lpm_trie_128`
    
    ## Documentation
    
    Comments in new unit testing functions, and more documentation added in LPM and bitmap code.
    
    ## Installation
    
    N/A
    
    Related work items: #159137, #159144, #159270, #159273

--- libs/runtime/ebpf_bitmap.h
+++ libs/runtime/ebpf_bitmap.h
@@ -73,6 +73,24 @@ extern "C"
     ebpf_bitmap_start_forward_search(_In_ const ebpf_bitmap_t* bitmap, _Out_ ebpf_bitmap_cursor_t* cursor);
 
     /**
+     * @brief Initialize a cursor to perform a forward scan starting from a specified bit.
+     *
+     * This function initializes a cursor for a forward search in the bitmap.
+     * The search starts at the specified bit.
+     * If the specified bit is beyond the end of the bitmap, the cursor is initialized
+     * to return no bits.
+     *
+     * @param[in] bitmap Pointer to the bitmap.
+     * @param[out] cursor Pointer to the cursor.
+     * @param[in] start_bit Bit to start searching from.
+     *
+     * @return void
+     */
+    void
+    ebpf_bitmap_start_forward_search_at(
+        _In_ const ebpf_bitmap_t* bitmap, _Out_ ebpf_bitmap_cursor_t* cursor, _In_ size_t start_bit);
+
+    /**
      * @brief Initialize a cursor to perform a reverse scan of bits.
      *
      * @param[in] bitmap Pointer to the bitmap.
@@ -80,6 +98,24 @@ extern "C"
      */
     void
     ebpf_bitmap_start_reverse_search(_In_ const ebpf_bitmap_t* bitmap, _Out_ ebpf_bitmap_cursor_t* cursor);
+
+    /**
+     * @brief Initialize a cursor to perform a reverse scan starting from a specified bit.
+     *
+     * This function initializes a cursor for a reverse search in the bitmap.
+     * The search starts at the specified bit.
+     * If the specified bit is beyond the end of the bitmap, the cursor is initialized
+     * to the last bit in the bitmap.
+     *
+     * @param[in] bitmap Pointer to the bitmap.
+     * @param[out] cursor Pointer to the cursor.
+     * @param[in] start_bit Bit to start searching from.
+     *
+     * @return void
+     */
+    void
+    ebpf_bitmap_start_reverse_search_at(
+        _In_ const ebpf_bitmap_t* bitmap, _Out_ ebpf_bitmap_cursor_t* cursor, _In_ size_t start_bit);
 
     /**
      * @brief Find the next set bit in the bitmap via forward search.
