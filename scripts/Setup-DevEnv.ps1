825605ffdc1b64d34202bdb73cc3a5d32c562ce7
Alan Jowett <alan.jowett@microsoft.com>
2024-08-06 10:54:55 -0700

    Update hash and set path to use environment variable
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

--- scripts/Setup-DevEnv.ps1
+++ scripts/Setup-DevEnv.ps1
@@ -2,13 +2,13 @@
 # SPDX-License-Identifier: MIT
 
 Invoke-WebRequest 'https://community.chocolatey.org/install.ps1' -OutFile $env:TEMP\install_choco.ps1
-if ((get-filehash -Algorithm SHA256 $env:TEMP\install_choco.ps1).Hash -ne 'B2980C92C1E3EFB45E3EFA428E2EF26EAC846F8B2606DA0D2B1342AC26D36B97') { throw "Wrong file hash for Chocolatey installer"}
+if ((get-filehash -Algorithm SHA256 $env:TEMP\install_choco.ps1).Hash -ne '44E045ED5350758616D664C5AF631E7F2CD10165F5BF2BD82CBF3A0BB8F63462') { throw "Wrong file hash for Chocolatey installer"}
 &"$env:TEMP\install_choco.ps1"
 choco install git --version 2.38.1 -y
 choco install visualstudio2022community --version 117.4.2.0 -y
 choco install visualstudio2022-workload-nativedesktop --version 1.0.0 -y
 choco install visualstudio2022buildtools --version 117.4.2.0 -y
-vs_installer.exe modify --installpath "C:\Program Files\Microsoft Visual Studio\2022\Community" --add Microsoft.VisualStudio.Component.VC.Llvm.ClangToolset --quiet
+vs_installer.exe modify --installpath $Env:ProgramFiles\Microsoft Visual Studio\2022\Community" --add Microsoft.VisualStudio.Component.VC.Llvm.ClangToolset --quiet
 
 # WDK for Windows 11 not currently available on Chocolate!
 #choco install windowsdriverkit11 --version 10.0.22621.382 -y
