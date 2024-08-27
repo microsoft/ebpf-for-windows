70d8bad4feb66e1d186dd7fa0a2db9ce7c69c824
Alan Jowett <alanjo@microsoft.com>
2024-08-27 09:30:12 -0700

    Stamp all tools with version information. (#3792)
    * Stamp all tools with version information.
    
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    
    * Add commit id to nuget package
    
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    
    * Move versioning to Directory.Build.props
    
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    
    * Set correct project type in sample.vcxproj
    
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    
    * Add version info to fuzz tests
    
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    
    * PR feedback
    
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    
    * Update tests/socket/resource.h
    
    Co-authored-by: Dave Thaler <dthaler1968@gmail.com>
    
    * Revert editor change
    
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    
    ---------
    
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    Co-authored-by: Alan Jowett <alan.jowett@microsoft.com>
    Co-authored-by: Dave Thaler <dthaler1968@gmail.com>

---
+++ tests/libfuzzer/netebpfext_fuzzer/resource.h
@@ -0,0 +1,5 @@
+// Copyright (c) eBPF for Windows contributors
+// SPDX-License-Identifier: MIT
+
+#define EBPF_FILE_DESCRIPTION "NetEbpfExt fuzz test"
+#define EBPF_FILE_NAME "netebpfext_fuzzer.exe"
---
+++ tests/sample/ext/app/resource.h
@@ -0,0 +1,5 @@
+// Copyright (c) eBPF for Windows contributors
+// SPDX-License-Identifier: MIT
+
+#define EBPF_FILE_DESCRIPTION "Sample extension tests"
+#define EBPF_FILE_NAME "sample_ext_app.exe"
