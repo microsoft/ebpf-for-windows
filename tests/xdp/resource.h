8c838d1ab5000f4726387ad42edc2d5189e0ad4d
Alan Jowett <alanjo@microsoft.com>
2024-08-27 09:30:12 -0700

    Stamp all tools with version information. (#3792)
    * Stamp all tools with version information.
    
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    
    * Add commit id to nuget package
    
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    
    * Move versioning to Directory.Build.props
    
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    
    * Set correct project type in sample.vcxproj
    
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    
    * Add version info to fuzz tests
    
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    
    * PR feedback
    
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    
    * Update tests/socket/resource.h
    
    Co-authored-by: Dave Thaler <dthaler1968@gmail.com>
    
    * Revert editor change
    
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    
    ---------
    
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    Co-authored-by: Alan Jowett <alan.jowett@microsoft.com>
    Co-authored-by: Dave Thaler <dthaler1968@gmail.com>

---
+++ tests/xdp/resource.h
@@ -0,0 +1,5 @@
+// Copyright (c) eBPF for Windows contributors
+// SPDX-License-Identifier: MIT
+
+#define EBPF_FILE_DESCRIPTION "XDP tests"
+#define EBPF_FILE_NAME "xdp_tests.exe"
---
+++ tools/bpftool/resource.h
@@ -0,0 +1,5 @@
+// Copyright (c) eBPF for Windows contributors
+// SPDX-License-Identifier: MIT
+
+#define EBPF_FILE_DESCRIPTION "Utility for interacting with eBPF for Windows"
+#define EBPF_FILE_NAME "bpftool.exe"
