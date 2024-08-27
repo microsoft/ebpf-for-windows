2473117b64e924e82431a2e5b64b1795833ec4c4
Alan Jowett <alan.jowett@microsoft.com>
2024-08-26 18:01:06 -0700

    Move versioning to Directory.Build.props
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

--- scripts/Set-Version.ps1
+++ scripts/Set-Version.ps1
@@ -6,7 +6,7 @@ param ($InputFile, $OutputFile, [parameter(Mandatory=$false)]$VCToolsRedistDir)
 # The git commit ID is in the include directory and is in the format:
 # #define GIT_COMMIT_ID "some commit id"
 $git_commit_id = Get-Content -Path "$PSScriptRoot\..\include\git_commit_id.h" -Raw -Encoding UTF8
-$git_commit_id = $git_commit_id[0].Split('"')[1]
+$git_commit_id = $git_commit_id.Split('"')[1]
 
 $version = &"$PSScriptRoot\Get-Version.ps1"
 $content = Get-Content $InputFile
---
+++ tests/socket/resource.h
@@ -0,0 +1,5 @@
+// Copyright (c) eBPF for Windows contributors
+// SPDX-License-Identifier: MIT
+
+#define EBPF_FILE_DESCRIPTION "Socket test"
+#define EBPF_FILE_NAME "socket_tests.exe"
