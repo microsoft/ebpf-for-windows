e75c3ca36117d9c85142718306e4f7e5242f3b94
Michael Friesen <3517159+mtfriesen@users.noreply.github.com>
2024-09-16 16:28:15 -0400

    fix fuzzer flavor

--- libs/shared/ebpf_etw.h
+++ libs/shared/ebpf_etw.h
@@ -6,8 +6,18 @@
 #pragma once
 
 #ifndef _KERNEL_MODE
-#define MCGEN_CONTROL_CALLBACK 1
-#define McGenControlCallbackV2 NULL /* TODO define a dummy fn */
+// The usersim configuration fails to load these constants from evntrace.h, so
+// redefine them here.
+#include <evntrace.h>
+#ifndef EVENT_CONTROL_CODE_DISABLE_PROVIDER
+#define EVENT_CONTROL_CODE_DISABLE_PROVIDER 0
 #endif
+#ifndef EVENT_CONTROL_CODE_ENABLE_PROVIDER
+#define EVENT_CONTROL_CODE_ENABLE_PROVIDER 1
+#endif
+#ifndef EVENT_CONTROL_CODE_CAPTURE_STATE
+#define EVENT_CONTROL_CODE_CAPTURE_STATE 2
+#endif
+#endif // _KERNEL_MODE
 
 #include <ebpf_etw_gen.h>
