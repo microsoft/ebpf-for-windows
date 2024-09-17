ce68e3e7d6e8c26eac680ca9e7f3b6040f76e849
Michael Friesen <3517159+mtfriesen@users.noreply.github.com>
2024-09-17 06:59:59 -0400

    try without usermode init/uninit

--- libs/shared/ebpf_etw.h
+++ libs/shared/ebpf_etw.h
@@ -6,18 +6,14 @@
 #pragma once
 
 #ifndef _KERNEL_MODE
-// The usersim configuration fails to load these constants from evntrace.h, so
-// redefine them here.
-#include <evntrace.h>
-#ifndef EVENT_CONTROL_CODE_DISABLE_PROVIDER
-#define EVENT_CONTROL_CODE_DISABLE_PROVIDER 0
+#define MCGEN_CONTROL_CALLBACK 1
 #endif
-#ifndef EVENT_CONTROL_CODE_ENABLE_PROVIDER
-#define EVENT_CONTROL_CODE_ENABLE_PROVIDER 1
-#endif
-#ifndef EVENT_CONTROL_CODE_CAPTURE_STATE
-#define EVENT_CONTROL_CODE_CAPTURE_STATE 2
-#endif
-#endif // _KERNEL_MODE
 
 #include <ebpf_etw_gen.h>
+
+#ifndef _KERNEL_MODE
+#undef EventRegisterEbpfForWindowsProvider
+#define EventRegisterEbpfForWindowsProvider() STATUS_SUCCESS
+#undef EventUnregisterEbpfForWindowsProvider
+#define EventUnregisterEbpfForWindowsProvider()
+#endif
