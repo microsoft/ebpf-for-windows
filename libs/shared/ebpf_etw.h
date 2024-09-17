e5379c3bdabf53daa1f7bffc870c9d40771bdce8
Michael Friesen <3517159+mtfriesen@users.noreply.github.com>
2024-09-17 10:27:20 -0400

    try um apis

--- libs/shared/ebpf_etw.h
+++ libs/shared/ebpf_etw.h
@@ -6,15 +6,17 @@
 #pragma once
 
 #ifndef _KERNEL_MODE
-#define MCGEN_CONTROL_CALLBACK 1
+#define EVENT_CONTROL_CODE_DISABLE_PROVIDER 0
+#define EVENT_CONTROL_CODE_ENABLE_PROVIDER 1
+#define EVENT_CONTROL_CODE_CAPTURE_STATE 2
 #endif
 
 #include <ebpf_etw_gen.h>
 
-#ifndef _KERNEL_MODE
-#undef EventRegisterEbpfForWindowsProvider
-#define EventRegisterEbpfForWindowsProvider() STATUS_SUCCESS
-#undef EventUnregisterEbpfForWindowsProvider
-#define EventUnregisterEbpfForWindowsProvider()
-#undef MCGEN_CONTROL_CALLBACK
-#endif
+// #ifndef _KERNEL_MODE
+// #undef EventRegisterEbpfForWindowsProvider
+// #define EventRegisterEbpfForWindowsProvider() STATUS_SUCCESS
+// #undef EventUnregisterEbpfForWindowsProvider
+// #define EventUnregisterEbpfForWindowsProvider()
+// #undef MCGEN_CONTROL_CALLBACK
+// #endif
