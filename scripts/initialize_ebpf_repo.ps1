6e11037d3b21cafa487741d6b6363c03554e6801
Alan Jowett <alan.jowett@microsoft.com>
2024-10-04 13:38:10 -0700

    PR feedback
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

--- scripts/initialize_ebpf_repo.ps1
+++ scripts/initialize_ebpf_repo.ps1
@@ -1,7 +1,7 @@
 # Copyright (c) eBPF for Windows contributors
 # SPDX-License-Identifier: MIT
 
-# Accept architecture as a parameter
+# Accept architecture as a parameter (e.g., X64, ARM64)
 param (
     [parameter(Mandatory = $false)][string] $Architecture = "x64"
 )
@@ -16,11 +16,12 @@ $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 Set-Location $scriptPath\..
 
 # Define the commands to run
+$cmakeCommonArgs = "-G 'Visual Studio 17 2022' -A $Architecture"
 $commands = @(
     "git submodule update --init --recursive",
-    "cmake -G 'Visual Studio 17 2022' -S external\ebpf-verifier -B external\ebpf-verifier\build -A $Architecture",
-    "cmake -G 'Visual Studio 17 2022' -S external\catch2 -B external\catch2\build -DBUILD_TESTING=OFF -A $Architecture",
-    "cmake -G 'Visual Studio 17 2022' -S external\ubpf -B external\ubpf\build -A $Architecture",
+    "cmake $cmakeCommonArgs -S external\ebpf-verifier -B external\ebpf-verifier\build",
+    "cmake $cmakeCommonArgs -S external\catch2 -B external\catch2\build -DBUILD_TESTING=OFF",
+    "cmake $cmakeCommonArgs -S external\ubpf -B external\ubpf\build",
     "nuget restore ebpf-for-windows.sln"
 )
 
--- scripts/onebranch/pre-build.ps1
+++ scripts/onebranch/pre-build.ps1
@@ -1,7 +1,7 @@
 # Copyright (c) eBPF for Windows contributors
 # SPDX-License-Identifier: MIT
 
-$OneBranchArch = $env:ONEBRANCH_ARCH
+$OneBranchArch = if ($env:ONEBRANCH_ARCH) { $env:ONEBRANCH_ARCH } else { "x64" }
 
 # Get the path where this script is located
 $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
@@ -9,6 +9,11 @@ $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 # Change the parent directory for the script directory
 Set-Location $scriptPath\..\..
 
-.\scripts\initialize_ebpf_repo.ps1 -Architecture $OneBranchArch
+try {
+    .\scripts\initialize_ebpf_repo.ps1 -Architecture $OneBranchArch
+}
+catch {
+    throw "Failed to initialize the eBPF for Windows repository."
+}
 
 Get-ChildItem -Path ./external -Filter *.dll -Recurse | Remove-Item
