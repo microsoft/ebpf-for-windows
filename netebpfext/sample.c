f5013ed7ab69d0af489b6b1d74463c8b42a72b79
Venkata Naga Kartik Pidatala <vpidatala@microsoft.com>
2024-12-30 13:03:55 -0800

    upload test#1

---
+++ netebpfext/sample.c
@@ -0,0 +1,22 @@
+// Copyright (c) eBPF for Windows contributors
+// SPDX-License-Identifier: MIT
+
+/**
+ * @file This file contains the definition of the layer GUIDs
+ *
+ */
+#include "platform.h"
+
+#include <ws2def.h>
+#include <ws2ipdef.h>
+#include <ndis.h>
+#include <../km/fwpmk.h>
+#include <../km/fwpsk.h>
+#include <../km/netioddk.h>
+#include <dpfilter.h>
+#include <fwpmtypes.h>
+#include <fwpstypes.h>
+#include <fwpvi.h>
+#include <iphlpapi.h>
+#include <netiodef.h>
+#include <winnt.h>
