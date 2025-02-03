6d077a23aca4b8176417eab7d1615bb24b3de7ac
Matt Ige <maige@microsoft.com>
2025-02-03 08:22:47 -0800

    fix issue with copying file

--- tests/stress/km/stress_tests_km.cpp
+++ tests/stress/km/stress_tests_km.cpp
@@ -630,52 +630,37 @@ _test_thread_function(thread_context& context)
         context.thread_index);
 }
 
-static _Must_inspect_result_ std::string
-_make_unique_file_copy(const std::string& file_name, uint32_t token)
+static std::string
+_get_unique_file_name(const std::string& file_name)
 {
-    // Generate the new (unique) file name.
-    std::filesystem::path file_spec = file_name;
-    std::string new_file_name = file_spec.stem().string();
-    new_file_name += (std::to_string(token) + file_spec.extension().string());
-    REQUIRE(new_file_name.size() != file_name.size());
+    uint32_t max_retries = 5;
+    while (max_retries > 0) {
+        // Generate the new (unique) file name.
+        std::filesystem::path file_spec = file_name;
+        std::string new_file_name = file_spec.stem().string();
+        // Use tick count to help generate a unique file name.
+        new_file_name += (std::to_string(GetTickCount64()) + file_spec.extension().string());
+        REQUIRE(new_file_name.size() != file_name.size());
 
-    if (std::filesystem::exists(new_file_name)) {
-        LOG_INFO("Target file already exist - {}", new_file_name);
-    }
-
-    // Make a copy. Retry a few times in case the file is in use.
-    bool result = false;
-    uint32_t max_retries = 50;
-    for (uint32_t i = 0; i < max_retries; i++) {
-        try {
-            if (std::filesystem::exists(new_file_name)) {
-                LOG_INFO("File already exists. Attempting to remove it - {}", new_file_name);
-                if (std::filesystem::remove(new_file_name)) {
-                    LOG_INFO("Removed existing file - {}", new_file_name);
-                } else {
-                    LOG_ERROR("Failed to remove existing file - {}", new_file_name);
-                }
-            }
-
-            LOG_INFO("Copying {} to {}", file_name, new_file_name);
-            result =
-                std::filesystem::copy_file(file_name, new_file_name, std::filesystem::copy_options::overwrite_existing);
-            if (result) {
-                break;
-            }
-            Sleep(10000);
-        } catch (const std::exception& e) {
-            if (i == (max_retries - 1)) {
-                LOG_ERROR(
-                    "FATAL ERROR: Failed to copy {} to {}. Error: {} after retry limit reached.",
-                    file_name,
-                    new_file_name,
-                    e.what());
-            }
+        if (!std::filesystem::exists(new_file_name)) {
+            return new_file_name;
+        } else {
+            LOG_INFO("Target file already exist - {}", new_file_name);
         }
     }
-    REQUIRE(result == true);
+}
 
+static _Must_inspect_result_ std::string
+_make_unique_file_copy(const std::string& file_name)
+{
+    std::new_file_name = _get_unique_file_name(file_name);
+    result = std::filesystem::copy_file(file_name, new_file_name, std::filesystem::copy_options::overwrite_existing);
+    if (result) {
+        LOG_INFO("Copied {} to {}", file_name, new_file_name);
+    } else {
+        LOG_ERROR("Failed to copy {} to {}", file_name, new_file_name);
+    }
+    REQUIRE(result == true);
     return new_file_name;
 }
 
@@ -794,8 +779,7 @@ _mt_prog_load_stress_test(ebpf_execution_type_t program_type, const test_control
                 if (test_control_info.use_unique_native_programs && context_entry.role == thread_role_type::CREATOR) {
 
                     // Create unique native programs for 'creator' threads only.
-                    context_entry.file_name =
-                        _make_unique_file_copy(program_attribs.native_file_name, (compartment_id - 1));
+                    context_entry.file_name = _make_unique_file_copy(program_attribs.native_file_name);
                 } else {
 
                     // Use the same file name for all 'creator' threads
