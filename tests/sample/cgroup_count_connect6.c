eae48c413cf50671b90749d9adcf86918c8f9f04
Matt Ige <maige@microsoft.com>
2025-02-05 18:16:11 -0800

    revert some test changes

--- tests/sample/cgroup_count_connect6.c
+++ tests/sample/cgroup_count_connect6.c
@@ -30,7 +30,6 @@ SEC("cgroup/connect6")
 int
 count_tcp_connect6(bpf_sock_addr_t* ctx)
 {
-    bpf_printk("maige count_tcp_connect6 invoked");
     int retval = 0;
     if (ctx->protocol != IPPROTO_TCP) {
         retval = BPF_SOCK_ADDR_VERDICT_PROCEED;
@@ -47,12 +46,12 @@ count_tcp_connect6(bpf_sock_addr_t* ctx)
     uint16_t key = remote_port;
     uint64_t value = 0;
     uint64_t* count = bpf_map_lookup_elem(&connect6_count_map, &key);
-    if (count != NULL) {
-        value = *count;
+    if (!count) {
+        value = 1;
+        bpf_map_update_elem(&connect6_count_map, &key, &value, 0);
+    } else {
+        *count += 1;
     }
-    ++value;
-    bpf_map_update_elem(&connect6_count_map, &key, &value, 0);
-    bpf_printk("maige count_tcp_connect6 map updated %d", value);
 
     // Fail all connect attempts at our port.  This ensures that we are invoked for every connect attempt as we need
     // to show steady increments in the count in our 'connect attempts' map.  Our user mode counterpart monitors this
