4d0e590c5eba65853ccafb56ea7e8e6a42e1d1cd
Matt Ige <maige@microsoft.com>
2025-02-11 12:56:21 -0800

    refactor safe require to common

--- tests/libs/util/misc_helper.h
+++ tests/libs/util/misc_helper.h
@@ -6,6 +6,7 @@
  */
 
 #pragma once
+#include <atomic>
 
 /**
  * @brief Helper function to get a 64-bit number where high-order 32 bits contain the process ID and
@@ -13,3 +14,20 @@
  */
 uint64_t
 get_current_pid_tgid();
+
+thread_local bool _is_main_thread = false;
+
+struct test_failure : std::exception
+{
+    test_failure(const std::string& message) : message(message) {}
+    std::string message;
+};
+
+#define SAFE_REQUIRE(x)                                               \
+    if (_is_main_thread) {                                            \
+        REQUIRE(x);                                                   \
+    } else {                                                          \
+        if (!(x)) {                                                   \
+            throw test_failure("Condition failed" + std::string(#x)); \
+        }                                                             \
+    }
