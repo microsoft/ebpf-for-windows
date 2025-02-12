a81fc79207e79bd1fbe083747593d50c34428a73
Lorenz Bauer <lmb@isovalent.com>
2025-02-12 03:05:15 -0800

    Fix race condition in _invalid_parameter_suppression
    Invalid parameter suppression currently sets the global invalid
    parameter handler. This means that concurrent execution may lead
    to executing the wrong handler.
    
    Instead, always set and restore the thread local handler.

--- libs/thunk/windows/platform.cpp
+++ libs/thunk/windows/platform.cpp
@@ -17,9 +17,9 @@ class _invalid_parameter_suppression
     _invalid_parameter_suppression()
     {
         _CrtSetReportMode(_CRT_ASSERT, 0);
-        previous_handler = _set_invalid_parameter_handler(_ignore_invalid_parameter);
+        previous_handler = _set_thread_local_invalid_parameter_handler(_ignore_invalid_parameter);
     }
-    ~_invalid_parameter_suppression() { _set_invalid_parameter_handler(previous_handler); }
+    ~_invalid_parameter_suppression() { _set_thread_local_invalid_parameter_handler(previous_handler); }
 
   private:
     static void
