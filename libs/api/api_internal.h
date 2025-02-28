d199ce7b5c3c86e7240342dcf85154bb95024020
Anadi Sharma <aanadi@cisco.com>
2025-02-27 21:15:52 -0500

    Review Comments
    Co-authored-by: Dave Thaler <dthaler1968@gmail.com>

--- libs/api/api_internal.h
+++ libs/api/api_internal.h
@@ -468,7 +468,7 @@ ebpf_get_link_fd_by_id(ebpf_id_t id, _Out_ int* fd) noexcept;
 /**
  * @brief Get an object's handle based on its fd.
  *
- * @param[in] File Descriptor of the object.
+ * @param[in] fd File descriptor of the object.
  * @param[out] ebpf_handle_t OS based handle to the object.
  *
  * @retval EBPF_SUCCESS The operation was successful.
