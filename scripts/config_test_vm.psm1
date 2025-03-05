e243d49f0914b30d583e52e377706882d69f8fee
Matt Ige (from Dev Box) <maige@microsoft.com>
2025-03-04 16:53:59 -0800

    wip

--- scripts/config_test_vm.psm1
+++ scripts/config_test_vm.psm1
@@ -189,11 +189,17 @@ function Export-BuildArtifactsToVMs
         } else {
             try {
                 Invoke-Command -VMName $VMName -Credential $TestCredential -ScriptBlock {
-                    Write-Host "Creating working directory c:\eBPF"
-                    # Create working directory c:\eBPF.
-                    if(!(Test-Path "$Env:SystemDrive\eBPF")) {
-                        New-Item -ItemType Directory -Path "$Env:SystemDrive\eBPF"
+                    #check if systemdrive is empty
+                    if ($Env:SystemDrive -eq "") {
+                        Write-Host "SystemDrive is empty. Setting it to C:"
+                        $drive = "C:"
+                    } else {
+                        $drive = $Env:SystemDrive
                     }
+                    Write-Host "Creating working directory $drive\eBPF"
+                    # Create working directory c:\eBPF.
+                    New-Item -ItemType Directory -Path "$drive\eBPF" -ErrorAction Ignore
+
                     # Write-Host "Adding registry path"
                     # # Enable EULA for all SysInternals tools.
                     # $RegistryPath = 'HKCU:\Software\Sysinternals'
