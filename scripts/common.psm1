caf3e07c81e95bba751c60211716d4df966071b3
Anurag Saxena <anusa@microsoft.com>
2025-04-25 17:17:01 -0700

    update regression tests

--- scripts/common.psm1
+++ scripts/common.psm1
@@ -501,7 +501,7 @@ function Get-RegressionTestArtifacts
 
     # Download regression test artifacts for each version.
     $DownloadPath = "$RegressionTestArtifactsPath"
-    $ArtifactName = "Release-v$ArtifactVersion/Build-x64.$Configuration.zip"
+    $ArtifactName = "Release-v$ArtifactVersion/Build.$Configuration.x64.zip"
     $ArtifactUrl = "https://github.com/microsoft/ebpf-for-windows/releases/download/" + $ArtifactName
 
     if (Test-Path -Path $DownloadPath\Build-x64.$Configuration) {
