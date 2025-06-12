a3d46531a68cac0d9f00efc9fe013ba79de12b16
Alan Jowett <alanjo@microsoft.com>
2025-06-12 07:57:59 -0700

    Fix build
    Signed-off-by: Alan Jowett <alanjo@microsoft.com>

--- scripts/setup_ebpf_cicd_tests.ps1
+++ scripts/setup_ebpf_cicd_tests.ps1
@@ -129,7 +129,7 @@ $Job = Start-Job -ScriptBlock {
     $LogFileName,
     $WorkingDirectory,
     $KmTracing,
-    $KmTraceType.
+    $KmTraceType,
     $EnableHVCI)
 
 # wait for the job to complete
