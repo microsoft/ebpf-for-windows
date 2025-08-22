4ef5d84bdde82546bd156c2303d16b99562b606f
Elaine Houha <elhouha@microsoft.com>
2025-08-22 15:26:29 -0700

    WIp commit

--- tests/sample/process_start_key.c
+++ tests/sample/process_start_key.c
@@ -4,17 +4,17 @@
 #include "bpf_endian.h"
 #include "bpf_helpers.h"
 
-struct value
+struct val
 {
     uint32_t current_pid;
     uint64_t start_key;
-} value;
+} val;
 
 struct
 {
     __uint(type, BPF_MAP_TYPE_ARRAY);
     __type(key, uint32_t);
-    __type(value, struct value);
+    __type(value, struct val);
     __uint(max_entries, 1);
 } process_start_key_map SEC(".maps");
 
@@ -28,9 +28,9 @@ func(bpf_sock_ops_t* ctx)
     {
         uint64_t start_key = bpf_get_current_process_start_key();
         uint64_t pid_tgid = bpf_get_current_pid_tgid();
-        struct value value = {.current_pid = pid_tgid >> 32, .start_key = start_key};
+        struct val v = {.current_pid = pid_tgid >> 32, .start_key = start_key};
         uint32_t key = 0;
-        bpf_map_update_elem(&process_start_key_map, &key, &value, 0);
+        bpf_map_update_elem(&process_start_key_map, &key, &v, 0);
     }
 
     return 0;
