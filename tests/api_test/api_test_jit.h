667c27911ae382f5bd0862e2b14e13a4f20a1fc2
Nicholas L <79675390+nmlud21@users.noreply.github.com>
2025-09-23 11:06:52 -0700

    Moved JIT specific #ifdef into separate files (#4597)
    * moved CONFIG_BPF_JIT_DISABLED blocks to secondary files
    
    Removed certain CONFIG_BPF_JIT_DISABLED from ebpf_core.c and consolidated logic into ebpf_core_config.c
    
    Refactored execution_context_unit_test.ccp and moved redundant defined(CONFIG_BPF_JIT_DISABLED) blocks to a different file
    
    Tests that are no-op when JIT is disabled only run when JIT is disabled
    
    Refactored libbpf_test.ccp and moved redundant defined(CONFIG_BPF_JIT_DISABLED) blocks to a different file
    
    Removed unecessary functions header from execution context unit test header file
    
    Refactored end_to_end.cpp and moved JIT config specific code to a new file.
    
    Moved JIT config specific code from api_test.cpp to api_test_jit.cpp
    
    Fixed issue where defining multiple function bodies in ebpf_core based on JIT config casued compiler issues.
    
    Fixed multiple issues where declaring function headers in JIT_CONFIG blocks was causing missing definitions.
    
    Changed function names that did not follow non-static naming conventions.
    
    Resolved comments and separated non JIT specific code more clearly.
    
    Resolved include errors in api_test.h.
    
    moved CONFIG_BPF_JIT_DISABLED blocks to secondary files
    
    Removed certain CONFIG_BPF_JIT_DISABLED from ebpf_core.c and consolidated logic into ebpf_core_config.c
    
    Removed certain CONFIG_BPF_JIT_DISABLED from ebpf_core.c and consolidated logic into ebpf_core_config.c
    
    Tests that are no-op when JIT is disabled only run when JIT is disabled
    
    Refactored libbpf_test.ccp and moved redundant defined(CONFIG_BPF_JIT_DISABLED) blocks to a different file
    
    Refactored end_to_end.cpp and moved JIT config specific code to a new file.
    
    Moved JIT config specific code from api_test.cpp to api_test_jit.cpp
    
    Fixed issue where defining multiple function bodies in ebpf_core based on JIT config casued compiler issues.
    
    Fixed multiple issues where declaring function headers in JIT_CONFIG blocks was causing missing definitions.
    
    Changed function names that did not follow non-static naming conventions.
    
    Resolved comments and separated non JIT specific code more clearly.
    
    Resolved include errors in api_test.h.
    
    * Fixed error cause by merge conflict.
    
    * Removed redundant function header in api_test_jit.h.
    
    * Fixed multiple header definitions in end_to_end.
    
    * Resolved comments in ebpf_core.
    
    ---------
    
    Co-authored-by: Nicholas Ludwig <nludwig@microsoft.com>

---
+++ tests/api_test/api_test_jit.h
@@ -0,0 +1,43 @@
+// Copyright (c) eBPF for Windows contributors
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include "api_internal.h"
+#include "api_test.h"
+#include "bpf/libbpf.h"
+#include "catch_wrapper.hpp"
+#include "ebpf_structs.h"
+#include "service_helper.h"
+
+#define SAMPLE_PROGRAM_COUNT 1
+#define BIND_MONITOR_PROGRAM_COUNT 1
+
+#define SAMPLE_MAP_COUNT 1
+#define BIND_MONITOR_MAP_COUNT 3
+
+#if !defined(CONFIG_BPF_JIT_DISABLED) || !defined(CONFIG_BPF_INTERPRETER_DISABLED)
+#define EBPF_SERVICE_BINARY_NAME L"ebpfsvc.exe"
+#define EBPF_SERVICE_NAME L"ebpfsvc"
+
+inline service_install_helper
+    ebpf_service_helper(EBPF_SERVICE_NAME, EBPF_SERVICE_BINARY_NAME, SERVICE_WIN32_OWN_PROCESS);
+#endif
+
+typedef struct _audit_entry
+{
+    uint64_t logon_id;
+    int32_t is_admin;
+} audit_entry_t;
+
+#if defined(CONFIG_BPF_JIT_DISABLED)
+#define JIT_LOAD_RESULT -ENOTSUP
+#else
+#define JIT_LOAD_RESULT 0
+#endif
+
+int32_t
+get_expected_jit_result(int32_t expected_result);
+
+void
+perform_socket_bind(const uint16_t test_port, bool expect_success);
