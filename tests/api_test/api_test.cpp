1d977a2c7f8460c00ed9908dc5e0b74b264b9e2c
Nicholas L <nickl239@att.net>
2025-12-01 08:48:12 -0800

    Added Info statement
    Signed-off-by: Nicholas L <nickl239@att.net>

--- tests/api_test/api_test.cpp
+++ tests/api_test/api_test.cpp
@@ -126,6 +126,7 @@ _test_multiple_programs_load(
         fd_t program_fd;
 
         result = program_load_helper(file_name, program_type, execution_type, &object, &program_fd);
+        INFO("Failed to load program: " << file_name << " with result: " << result);
         REQUIRE(expected_load_result == result);
         if (expected_load_result == 0) {
             REQUIRE(program_fd > 0);
