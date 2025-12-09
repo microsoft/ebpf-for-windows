edfe484c7afc1512a506125793b1f509a4c657a6
Vinod K L Swamy <vinodko@microsoft.com>
2025-12-08 17:57:06 -0800

    Fixes #4706: Bring back XDP Mock provider and test XDP programs

---
+++ tests/bpf2c_tests/expected/droppacket_sys.c
@@ -0,0 +1,469 @@
+// Copyright (c) eBPF for Windows contributors
+// SPDX-License-Identifier: MIT
+
+// Do not alter this generated file.
+// This file was generated from droppacket.o
+
+#define NO_CRT
+#include "bpf2c.h"
+
+#include <guiddef.h>
+#include <wdm.h>
+#include <wsk.h>
+
+DRIVER_INITIALIZE DriverEntry;
+DRIVER_UNLOAD DriverUnload;
+RTL_QUERY_REGISTRY_ROUTINE static _bpf2c_query_registry_routine;
+
+#define metadata_table droppacket##_metadata_table
+
+static GUID _bpf2c_npi_id = {/* c847aac8-a6f2-4b53-aea3-f4a94b9a80cb */
+                             0xc847aac8,
+                             0xa6f2,
+                             0x4b53,
+                             {0xae, 0xa3, 0xf4, 0xa9, 0x4b, 0x9a, 0x80, 0xcb}};
+static NPI_MODULEID _bpf2c_module_id = {sizeof(_bpf2c_module_id), MIT_GUID, {0}};
+static HANDLE _bpf2c_nmr_client_handle;
+static HANDLE _bpf2c_nmr_provider_handle;
+extern metadata_table_t metadata_table;
+
+static NTSTATUS
+_bpf2c_npi_client_attach_provider(
+    _In_ HANDLE nmr_binding_handle,
+    _In_ void* client_context,
+    _In_ const NPI_REGISTRATION_INSTANCE* provider_registration_instance);
+
+static NTSTATUS
+_bpf2c_npi_client_detach_provider(_In_ void* client_binding_context);
+
+static const NPI_CLIENT_CHARACTERISTICS _bpf2c_npi_client_characteristics = {
+    0,                                  // Version
+    sizeof(NPI_CLIENT_CHARACTERISTICS), // Length
+    _bpf2c_npi_client_attach_provider,
+    _bpf2c_npi_client_detach_provider,
+    NULL,
+    {0,                                 // Version
+     sizeof(NPI_REGISTRATION_INSTANCE), // Length
+     &_bpf2c_npi_id,
+     &_bpf2c_module_id,
+     0,
+     NULL}};
+
+static NTSTATUS
+_bpf2c_query_npi_module_id(
+    _In_ const wchar_t* value_name,
+    unsigned long value_type,
+    _In_ const void* value_data,
+    unsigned long value_length,
+    _Inout_ void* context,
+    _Inout_ void* entry_context)
+{
+    UNREFERENCED_PARAMETER(value_name);
+    UNREFERENCED_PARAMETER(context);
+    UNREFERENCED_PARAMETER(entry_context);
+
+    if (value_type != REG_BINARY) {
+        return STATUS_INVALID_PARAMETER;
+    }
+    if (value_length != sizeof(_bpf2c_module_id.Guid)) {
+        return STATUS_INVALID_PARAMETER;
+    }
+
+    memcpy(&_bpf2c_module_id.Guid, value_data, value_length);
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS
+DriverEntry(_In_ DRIVER_OBJECT* driver_object, _In_ UNICODE_STRING* registry_path)
+{
+    NTSTATUS status;
+    RTL_QUERY_REGISTRY_TABLE query_table[] = {
+        {
+            NULL,                      // Query routine
+            RTL_QUERY_REGISTRY_SUBKEY, // Flags
+            L"Parameters",             // Name
+            NULL,                      // Entry context
+            REG_NONE,                  // Default type
+            NULL,                      // Default data
+            0,                         // Default length
+        },
+        {
+            _bpf2c_query_npi_module_id,  // Query routine
+            RTL_QUERY_REGISTRY_REQUIRED, // Flags
+            L"NpiModuleId",              // Name
+            NULL,                        // Entry context
+            REG_NONE,                    // Default type
+            NULL,                        // Default data
+            0,                           // Default length
+        },
+        {0}};
+
+    status = RtlQueryRegistryValues(RTL_REGISTRY_ABSOLUTE, registry_path->Buffer, query_table, NULL, NULL);
+    if (!NT_SUCCESS(status)) {
+        goto Exit;
+    }
+
+    status = NmrRegisterClient(&_bpf2c_npi_client_characteristics, NULL, &_bpf2c_nmr_client_handle);
+
+Exit:
+    if (NT_SUCCESS(status)) {
+        driver_object->DriverUnload = DriverUnload;
+    }
+
+    return status;
+}
+
+void
+DriverUnload(_In_ DRIVER_OBJECT* driver_object)
+{
+    NTSTATUS status = NmrDeregisterClient(_bpf2c_nmr_client_handle);
+    if (status == STATUS_PENDING) {
+        NmrWaitForClientDeregisterComplete(_bpf2c_nmr_client_handle);
+    }
+    UNREFERENCED_PARAMETER(driver_object);
+}
+
+static NTSTATUS
+_bpf2c_npi_client_attach_provider(
+    _In_ HANDLE nmr_binding_handle,
+    _In_ void* client_context,
+    _In_ const NPI_REGISTRATION_INSTANCE* provider_registration_instance)
+{
+    NTSTATUS status = STATUS_SUCCESS;
+    void* provider_binding_context = NULL;
+    void* provider_dispatch_table = NULL;
+
+    UNREFERENCED_PARAMETER(client_context);
+    UNREFERENCED_PARAMETER(provider_registration_instance);
+
+    if (_bpf2c_nmr_provider_handle != NULL) {
+        return STATUS_INVALID_PARAMETER;
+    }
+
+    status = NmrClientAttachProvider(
+        nmr_binding_handle, client_context, &metadata_table, &provider_binding_context, &provider_dispatch_table);
+    if (status != STATUS_SUCCESS) {
+        goto Done;
+    }
+    _bpf2c_nmr_provider_handle = nmr_binding_handle;
+
+Done:
+    return status;
+}
+
+static NTSTATUS
+_bpf2c_npi_client_detach_provider(_In_ void* client_binding_context)
+{
+    _bpf2c_nmr_provider_handle = NULL;
+    UNREFERENCED_PARAMETER(client_binding_context);
+    return STATUS_SUCCESS;
+}
+
+#include "bpf2c.h"
+
+static void
+_get_hash(_Outptr_result_buffer_maybenull_(*size) const uint8_t** hash, _Out_ size_t* size)
+{
+    *hash = NULL;
+    *size = 0;
+}
+
+#pragma data_seg(push, "maps")
+static map_entry_t _maps[] = {
+    {
+     {0, 0},
+     {
+         1,                  // Current Version.
+         80,                 // Struct size up to the last field.
+         80,                 // Total struct size including padding.
+     },
+     {
+         BPF_MAP_TYPE_ARRAY, // Type of map.
+         4,                  // Size in bytes of a map key.
+         4,                  // Size in bytes of a map value.
+         1,                  // Maximum number of entries allowed in the map.
+         0,                  // Inner map index.
+         LIBBPF_PIN_NONE,    // Pinning type for the map.
+         10,                 // Identifier for a map template.
+         0,                  // The id of the inner map template.
+     },
+     "interface_index_map"},
+    {
+     {0, 0},
+     {
+         1,                  // Current Version.
+         80,                 // Struct size up to the last field.
+         80,                 // Total struct size including padding.
+     },
+     {
+         BPF_MAP_TYPE_ARRAY, // Type of map.
+         4,                  // Size in bytes of a map key.
+         8,                  // Size in bytes of a map value.
+         1,                  // Maximum number of entries allowed in the map.
+         0,                  // Inner map index.
+         LIBBPF_PIN_NONE,    // Pinning type for the map.
+         15,                 // Identifier for a map template.
+         0,                  // The id of the inner map template.
+     },
+     "dropped_packet_map"},
+};
+#pragma data_seg(pop)
+
+static void
+_get_maps(_Outptr_result_buffer_maybenull_(*count) map_entry_t** maps, _Out_ size_t* count)
+{
+    *maps = _maps;
+    *count = 2;
+}
+
+static void
+_get_global_variable_sections(
+    _Outptr_result_buffer_maybenull_(*count) global_variable_section_info_t** global_variable_sections,
+    _Out_ size_t* count)
+{
+    *global_variable_sections = NULL;
+    *count = 0;
+}
+
+static helper_function_entry_t DropPacket_helpers[] = {
+    {
+     {1, 40, 40}, // Version header.
+     1,
+     "helper_id_1",
+    },
+};
+
+static GUID DropPacket_program_type_guid = {
+    0xf1832a85, 0x85d5, 0x45b0, {0x98, 0xa0, 0x70, 0x69, 0xd6, 0x30, 0x13, 0xb0}};
+static GUID DropPacket_attach_type_guid = {
+    0x85e0d8ef, 0x579e, 0x4931, {0xb0, 0x72, 0x8e, 0xe2, 0x26, 0xbb, 0x2e, 0x9d}};
+static uint16_t DropPacket_maps[] = {
+    0,
+    1,
+};
+
+#pragma code_seg(push, "xdp")
+static uint64_t
+DropPacket(void* context, const program_runtime_context_t* runtime_context)
+#line 44 "sample/droppacket.c"
+{
+#line 44 "sample/droppacket.c"
+    // Prologue.
+#line 44 "sample/droppacket.c"
+    uint64_t stack[(UBPF_STACK_SIZE + 7) / 8];
+#line 44 "sample/droppacket.c"
+    register uint64_t r0 = 0;
+#line 44 "sample/droppacket.c"
+    register uint64_t r1 = 0;
+#line 44 "sample/droppacket.c"
+    register uint64_t r2 = 0;
+#line 44 "sample/droppacket.c"
+    register uint64_t r3 = 0;
+#line 44 "sample/droppacket.c"
+    register uint64_t r4 = 0;
+#line 44 "sample/droppacket.c"
+    register uint64_t r5 = 0;
+#line 44 "sample/droppacket.c"
+    register uint64_t r6 = 0;
+#line 44 "sample/droppacket.c"
+    register uint64_t r10 = 0;
+
+#line 44 "sample/droppacket.c"
+    r1 = (uintptr_t)context;
+#line 44 "sample/droppacket.c"
+    r10 = (uintptr_t)((uint8_t*)stack + sizeof(stack));
+
+#line 44 "sample/droppacket.c"
+    r6 = r1;
+#line 44 "sample/droppacket.c"
+    r1 = IMMEDIATE(0);
+#line 48 "sample/droppacket.c"
+    WRITE_ONCE_64(r10, (uint64_t)r1, OFFSET(-8));
+#line 48 "sample/droppacket.c"
+    r2 = r10;
+#line 48 "sample/droppacket.c"
+    r2 += IMMEDIATE(-8);
+#line 57 "sample/droppacket.c"
+    r1 = POINTER(runtime_context->map_data[0].address);
+#line 57 "sample/droppacket.c"
+    r0 = runtime_context->helper_data[0].address(r1, r2, r3, r4, r5, context);
+#line 57 "sample/droppacket.c"
+    if ((runtime_context->helper_data[0].tail_call) && (r0 == 0)) {
+#line 57 "sample/droppacket.c"
+        return 0;
+#line 57 "sample/droppacket.c"
+    }
+#line 57 "sample/droppacket.c"
+    r1 = r0;
+#line 58 "sample/droppacket.c"
+    if (r1 == IMMEDIATE(0)) {
+#line 58 "sample/droppacket.c"
+        goto label_1;
+#line 58 "sample/droppacket.c"
+    }
+#line 58 "sample/droppacket.c"
+    r0 = IMMEDIATE(1);
+#line 59 "sample/droppacket.c"
+    READ_ONCE_32(r1, r1, OFFSET(0));
+#line 59 "sample/droppacket.c"
+    READ_ONCE_32(r2, r6, OFFSET(24));
+#line 59 "sample/droppacket.c"
+    if (r2 != r1) {
+#line 59 "sample/droppacket.c"
+        goto label_2;
+#line 59 "sample/droppacket.c"
+    }
+label_1:
+#line 59 "sample/droppacket.c"
+    r0 = IMMEDIATE(1);
+#line 65 "sample/droppacket.c"
+    READ_ONCE_64(r1, r6, OFFSET(0));
+#line 65 "sample/droppacket.c"
+    READ_ONCE_64(r2, r6, OFFSET(8));
+#line 65 "sample/droppacket.c"
+    r3 = r1;
+#line 65 "sample/droppacket.c"
+    r3 += IMMEDIATE(42);
+#line 65 "sample/droppacket.c"
+    if (r3 > r2) {
+#line 65 "sample/droppacket.c"
+        goto label_2;
+#line 65 "sample/droppacket.c"
+    }
+#line 70 "sample/droppacket.c"
+    READ_ONCE_16(r3, r1, OFFSET(12));
+#line 70 "sample/droppacket.c"
+    if (r3 != IMMEDIATE(8)) {
+#line 70 "sample/droppacket.c"
+        goto label_2;
+#line 70 "sample/droppacket.c"
+    }
+#line 73 "sample/droppacket.c"
+    READ_ONCE_8(r3, r1, OFFSET(23));
+#line 73 "sample/droppacket.c"
+    if (r3 != IMMEDIATE(17)) {
+#line 73 "sample/droppacket.c"
+        goto label_2;
+#line 73 "sample/droppacket.c"
+    }
+#line 73 "sample/droppacket.c"
+    r1 += IMMEDIATE(14);
+#line 75 "sample/droppacket.c"
+    READ_ONCE_8(r3, r1, OFFSET(0));
+#line 75 "sample/droppacket.c"
+    r3 <<= (IMMEDIATE(2) & 63);
+#line 75 "sample/droppacket.c"
+    r3 &= IMMEDIATE(60);
+#line 75 "sample/droppacket.c"
+    r1 += r3;
+#line 76 "sample/droppacket.c"
+    r3 = r1;
+#line 76 "sample/droppacket.c"
+    r3 += IMMEDIATE(8);
+#line 76 "sample/droppacket.c"
+    if (r3 > r2) {
+#line 76 "sample/droppacket.c"
+        goto label_2;
+#line 76 "sample/droppacket.c"
+    }
+#line 80 "sample/droppacket.c"
+    READ_ONCE_16(r1, r1, OFFSET(4));
+#line 80 "sample/droppacket.c"
+    r1 = htobe16((uint16_t)r1);
+#line 80 "sample/droppacket.c"
+    r1 &= UINT32_MAX;
+#line 80 "sample/droppacket.c"
+    if (r1 > IMMEDIATE(8)) {
+#line 80 "sample/droppacket.c"
+        goto label_2;
+#line 80 "sample/droppacket.c"
+    }
+#line 80 "sample/droppacket.c"
+    r2 = r10;
+#line 81 "sample/droppacket.c"
+    r2 += IMMEDIATE(-8);
+#line 81 "sample/droppacket.c"
+    r1 = POINTER(runtime_context->map_data[1].address);
+#line 81 "sample/droppacket.c"
+    r0 = runtime_context->helper_data[0].address(r1, r2, r3, r4, r5, context);
+#line 81 "sample/droppacket.c"
+    if ((runtime_context->helper_data[0].tail_call) && (r0 == 0)) {
+#line 81 "sample/droppacket.c"
+        return 0;
+#line 81 "sample/droppacket.c"
+    }
+#line 81 "sample/droppacket.c"
+    r1 = r0;
+#line 81 "sample/droppacket.c"
+    r0 = IMMEDIATE(2);
+#line 82 "sample/droppacket.c"
+    if (r1 == IMMEDIATE(0)) {
+#line 82 "sample/droppacket.c"
+        goto label_2;
+#line 82 "sample/droppacket.c"
+    }
+#line 83 "sample/droppacket.c"
+    READ_ONCE_64(r2, r1, OFFSET(0));
+#line 83 "sample/droppacket.c"
+    r2 += IMMEDIATE(1);
+#line 83 "sample/droppacket.c"
+    WRITE_ONCE_64(r1, (uint64_t)r2, OFFSET(0));
+label_2:
+#line 91 "sample/droppacket.c"
+    return r0;
+#line 44 "sample/droppacket.c"
+}
+#pragma code_seg(pop)
+#line __LINE__ __FILE__
+
+#pragma data_seg(push, "programs")
+static program_entry_t _programs[] = {
+    {
+        0,
+        {1, 144, 144}, // Version header.
+        DropPacket,
+        "xdp",
+        "xdp",
+        "DropPacket",
+        DropPacket_maps,
+        2,
+        DropPacket_helpers,
+        1,
+        47,
+        &DropPacket_program_type_guid,
+        &DropPacket_attach_type_guid,
+    },
+};
+#pragma data_seg(pop)
+
+static void
+_get_programs(_Outptr_result_buffer_(*count) program_entry_t** programs, _Out_ size_t* count)
+{
+    *programs = _programs;
+    *count = 1;
+}
+
+static void
+_get_version(_Out_ bpf2c_version_t* version)
+{
+    version->major = 1;
+    version->minor = 1;
+    version->revision = 0;
+}
+
+static void
+_get_map_initial_values(_Outptr_result_buffer_(*count) map_initial_values_t** map_initial_values, _Out_ size_t* count)
+{
+    *map_initial_values = NULL;
+    *count = 0;
+}
+
+metadata_table_t droppacket_metadata_table = {
+    sizeof(metadata_table_t),
+    _get_programs,
+    _get_maps,
+    _get_hash,
+    _get_version,
+    _get_map_initial_values,
+    _get_global_variable_sections,
+};
