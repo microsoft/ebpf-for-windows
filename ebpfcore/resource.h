0d2cef7c5cc73373dde98a2ae6cc41b3b5ed9cbc
fred231084g <83791194+fred231084g@users.noreply.github.com>
2026-01-10 03:17:06 +0100

    Delete ebpfcore directory

--- ebpfcore/resource.h
+++
@@ -1,5 +0,0 @@
-// Copyright (c) eBPF for Windows contributors
-// SPDX-License-Identifier: MIT
-
-#define EBPF_FILE_DESCRIPTION "eBPF For Windows Execution Context"
-#define EBPF_FILE_NAME "ebpfcore.sys"
