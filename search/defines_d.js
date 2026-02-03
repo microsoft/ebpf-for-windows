bbedfc55db4eaa791202797cc868582b0b2c6d03
Github Action <ebpf-for-windows@users.noreply.github.com>
2026-02-03 21:02:25 +0000

    Updated documentation
    Signed-off-by: Github Action <ebpf-for-windows@users.noreply.github.com>

--- search/defines_d.js
+++ search/defines_d.js
@@ -4,5 +4,8 @@ var searchData=
   ['read_5fonce_5f32_1',['READ_ONCE_32',['../bpf2c_8h.html#a77a9342354895fbfc947a85d456f3ddb',1,'bpf2c.h']]],
   ['read_5fonce_5f64_2',['READ_ONCE_64',['../bpf2c_8h.html#a6276e6ecad80673e58fcbcebddd01b8b',1,'bpf2c.h']]],
   ['read_5fonce_5f8_3',['READ_ONCE_8',['../bpf2c_8h.html#ae96f62e9656ce0e6b42cb928acd1d3b4',1,'bpf2c.h']]],
-  ['ring_5fbuffer_5fopts_5f_5flast_5ffield_4',['ring_buffer_opts__last_field',['../libbpf_8h.html#a5bc343e99603bfe47f402cd8b6340795',1,'libbpf.h']]]
+  ['read_5fonce_5fs16_4',['READ_ONCE_S16',['../bpf2c_8h.html#a4dd886f2b9f1fe593914f1e966947366',1,'bpf2c.h']]],
+  ['read_5fonce_5fs32_5',['READ_ONCE_S32',['../bpf2c_8h.html#a16a39c7a17a934938fd816fb508f8054',1,'bpf2c.h']]],
+  ['read_5fonce_5fs8_6',['READ_ONCE_S8',['../bpf2c_8h.html#a287daebd9f35099e58b8914096a580eb',1,'bpf2c.h']]],
+  ['ring_5fbuffer_5fopts_5f_5flast_5ffield_7',['ring_buffer_opts__last_field',['../libbpf_8h.html#a5bc343e99603bfe47f402cd8b6340795',1,'libbpf.h']]]
 ];
