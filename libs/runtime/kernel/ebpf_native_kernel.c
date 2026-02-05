b62ef395211e65c5c2a424326b5edc81a1054b79
Nicholas Ludwig <nludwig@microsoft.com>
2026-02-05 15:52:36 -0800

    Added status return by ebpf_native_load_driver.
    Signed-off-by: Nicholas Ludwig <nludwig@microsoft.com>

--- libs/runtime/kernel/ebpf_native_kernel.c
+++ libs/runtime/kernel/ebpf_native_kernel.c
@@ -8,17 +8,13 @@ _Must_inspect_result_ ebpf_result_t
 ebpf_native_load_driver(_In_z_ const wchar_t* service_name)
 {
     UNICODE_STRING driver_service_name;
-    ebpf_result_t result = EBPF_SUCCESS;
     NTSTATUS status;
 
     RtlInitUnicodeString(&driver_service_name, service_name);
     status = ZwLoadDriver(&driver_service_name);
     EBPF_LOG_NTSTATUS_WSTRING_API(EBPF_TRACELOG_KEYWORD_NATIVE, service_name, ZwLoadDriver, status);
-    if (status != STATUS_SUCCESS) {
-        result = EBPF_FAILED;
-    }
 
-    return result;
+    EBPF_RETURN_RESULT(ntstatus_to_ebpf_result(status));
 }
 
 void
