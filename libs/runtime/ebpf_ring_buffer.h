15911ac4e253e2e5f500404a7e6443fd4da1149b
Michael Agun <danielagun@microsoft.com>
2026-02-18 09:12:56 -0800

    add comment for perf producer page

--- libs/runtime/ebpf_ring_buffer.h
+++ libs/runtime/ebpf_ring_buffer.h
@@ -67,9 +67,11 @@ ebpf_ring_buffer_producer_page_t*
 ebpf_ring_buffer_get_producer_page(_Inout_ ebpf_ring_buffer_t* ring_buffer);
 
 /**
- * @brief Get the shared producer page for the perf event array.
+ * @brief Get the shared producer page for a perf event array map.
  *
- * @param[in, out] ring_buffer Ring buffer to query.
+ * @note ring_buffer must correspond to a perf event array map ring.
+ *
+ * @param[in, out] ring_buffer Per-cpu perf event array map ring to query.
  * @retval Pointer to the producer page, or NULL if not found.
  */
 ebpf_perf_event_array_producer_page_t*
