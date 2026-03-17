6127d1789fcd468b6ce7b8eb46a22d4115e681ff
Github Action <ebpf-for-windows@users.noreply.github.com>
2026-03-17 18:44:14 +0000

    Updated documentation
    Signed-off-by: Github Action <ebpf-for-windows@users.noreply.github.com>

--- search/enums_2.js
+++ search/enums_2.js
@@ -6,5 +6,6 @@ var searchData=
   ['ebpf_5fpin_5ftype_3',['ebpf_pin_type',['../ebpf__structs_8h.html#a8a7e198659f6bcf766a110c6324e3a20',1,'ebpf_structs.h']]],
   ['ebpf_5fresult_4',['ebpf_result',['../ebpf__result_8h.html#a916ce31f9038720787b441792c088372',1,'ebpf_result.h']]],
   ['ebpf_5fring_5fbuffer_5fflags_5',['ebpf_ring_buffer_flags',['../ebpf__api_8h.html#a22c8510f176e45d5a47fdc32b4af0407',1,'ebpf_api.h']]],
-  ['ebpf_5fsock_5faddr_5fhelper_5fid_5ft_6',['ebpf_sock_addr_helper_id_t',['../ebpf__nethooks_8h.html#a3a163a4fff0e95e1a572099e4478620e',1,'ebpf_nethooks.h']]]
+  ['ebpf_5fsock_5faddr_5fhelper_5fid_5ft_6',['ebpf_sock_addr_helper_id_t',['../ebpf__nethooks_8h.html#a3a163a4fff0e95e1a572099e4478620e',1,'ebpf_nethooks.h']]],
+  ['ebpf_5fsock_5fops_5fhelper_5fid_5ft_7',['ebpf_sock_ops_helper_id_t',['../ebpf__nethooks_8h.html#a207d452687a292ec766bcdbfdcc0c386',1,'ebpf_nethooks.h']]]
 ];
