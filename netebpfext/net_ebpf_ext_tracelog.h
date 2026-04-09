28f7edc8baf7b9b446326abde9f71c1340c7a9d3
Laksh Kotian <lakshkotian@microsoft.com>
2026-04-09 11:40:15 -0700

    remove header

--- netebpfext/net_ebpf_ext_tracelog.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) eBPF for Windows contributors
-// SPDX-License-Identifier: MIT
-
-// This header forwards to the common ebpf_ext_tracelog.h from ebpf-extension-common.
-// Kept for include compatibility with existing source files.
-
-#pragma once
-
-#include "ebpf_ext_tracelog.h"
